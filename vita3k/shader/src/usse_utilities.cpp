72237f6ffb2442cdf43e94007a7b3856327742b1
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-06-22 20:29:15 +0800

    Update usse_utilities.cpp

--- vita3k/shader/src/usse_utilities.cpp
+++ vita3k/shader/src/usse_utilities.cpp
@@ -170,11 +170,11 @@ static spv::Function *make_fx10_unpack_func(spv::Builder &b, const SpirvUtilFunc
     spv::Block *fx10_unpack_func_block;
     spv::Block *last_build_point = b.getBuildPoint();
 
-    std::vector<spv::Id> type_i32 = b.makeIntType(32);
-    std::vector<spv::Id> ivec3 = b.makeVectorType(type_i32, 3);
-    std::vector<spv::Id> uvec3 = b.makeVectorType(b.makeUintType(32), 3);
-    std::vector<spv::Id> type_f32 = b.makeFloatType(32);
-    std::vector<spv::Id> type_f32_v3 = b.makeVectorType(type_f32, 3);
+    <spv::Id> type_i32 = b.makeIntType(32);
+    <spv::Id> ivec3 = b.makeVectorType(type_i32, 3);
+    <spv::Id> uvec3 = b.makeVectorType(b.makeUintType(32), 3);
+    <spv::Id> type_f32 = b.makeFloatType(32);
+    <spv::Id> type_f32_v3 = b.makeVectorType(type_f32, 3);
 
     spv::Function *fx10_unpack_func = b.makeFunctionEntry(
         spv::NoPrecision, type_f32_v3, "unpack3xFX10", { type_f32 }, { "to_unpack" },
@@ -259,7 +259,7 @@ static spv::Function *make_unpack_func(spv::Builder &b, const FeatureState &feat
     }
 
     spv::Function *unpack_func = b.makeFunctionEntry(
-        spv::NoPrecision, output_type, func_name.c_str(), { type_f32 }, { "to_unpack" },
+        spv::NoPrecision, output_type, func_name.c_str(), { addCapability(type_f32) }, { "to_unpack" },
         decorations, &unpack_func_block);
     unpack_func->setReturnPrecision(spv::DecorationRelaxedPrecision);
     spv::Id extracted = unpack_func->getParamId(0);
