49a08adcbe9ad12af9667a902ad192cbf00d9501
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-08-13 23:13:07 +0800

    Update interface.cpp

--- vita3k/interface.cpp
+++ vita3k/interface.cpp
@@ -572,16 +572,16 @@ static void take_screenshot(EmuEnvState &emuenv) {
             screenshot_ok = true;
     }
     if (screenshot_ok){
-        std::string tmp = "Successfully saved screenshot to {}" + save_file;
-        LOG_INFO("{}", tmp.c_str());
+        auto tmp = fmt::print("Successfully saved screenshot to {}", save_file);
+        LOG_INFO("{}", tmp);
 #ifdef ANDROID
-        SDL_AndroidShowToast(tmp.c_str(), 1, -1, 0, 0);
+        SDL_AndroidShowToast(tmp, 1, -1, 0, 0);
 #endif
     }else{
-        std::string tmp = "Failed to save screenshot";
-        LOG_INFO("{}", tmp.c_str());
+        auto tmp = "Failed to save screenshot";
+        LOG_INFO("{}", tmp);
 #ifdef ANDROID
-        SDL_AndroidShowToast(tmp.c_str(), 1, -1, 0, 0);
+        SDL_AndroidShowToast(tmp, 1, -1, 0, 0);
 #endif
     }
 }
