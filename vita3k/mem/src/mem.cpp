0d964d4e83672acfdb50378aeca840f786d60655
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-08-24 09:51:07 +0800

    Update mem.cpp

--- vita3k/mem/src/mem.cpp
+++ vita3k/mem/src/mem.cpp
@@ -72,18 +72,15 @@ bool init(MemState &state, const bool use_page_table) {
 #endif
     state.page_size = std::max(STANDARD_PAGE_SIZE, state.page_size);
 
-//    LOG_DEBUG("Default virtual Memory size: {} Bytes", TOTAL_MEM_SIZE);
-//    LOG_DEBUG("Default page size: {} Bytes", STANDARD_PAGE_SIZE);
-    size_t mem_size_tmp = SDL_GetSystemRAM();
-    mem_size_tmp = (mem_size_tmp - (mem_size_tmp / 4)) * 1000000;
-//    LOG_DEBUG("Custom memory size set: {} Bytes", mem_size_tmp);
+    auto mem_size_tmp = SDL_GetSystemRAM();
+    mem_size_tmp = (mem_size_tmp - (mem_size_tmp / 3)) * 1000000;
     if(mem_size_tmp < TOTAL_MEM_SIZE){
        LOG_DEBUG("Virtual Memory size too low!, using default value!");
     }else{
        TOTAL_MEM_SIZE = mem_size_tmp;
     }
-    LOG_DEBUG("Virtual Memory size set: {} Bytes", TOTAL_MEM_SIZE);
-//    LOG_DEBUG("Page size: {} Bytes", state.page_size);
+    mem_size_tmp = TOTAL_MEM_SIZE / 1000;
+    LOG_DEBUG("Virtual Memory size set: {} MB", mem_size_tmp);
     assert(state.page_size >= 4096); // Limit imposed by Unicorn.
     assert(!use_page_table || state.page_size == STANDARD_PAGE_SIZE);
 
