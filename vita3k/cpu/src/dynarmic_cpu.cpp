28a06ea2a693737ea1cf63bef578912e6156abb5
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-08-25 10:51:35 +0800

    Update dynarmic_cpu.cpp

--- vita3k/cpu/src/dynarmic_cpu.cpp
+++ vita3k/cpu/src/dynarmic_cpu.cpp
@@ -305,23 +305,25 @@ std::unique_ptr<Dynarmic::A32::Jit> DynarmicCPU::make_jit() {
     if (!log_mem && cpu_opt) {
         config.fastmem_pointer = std::bit_cast<uintptr_t>(parent->mem->memory.get());
     }
-    config.fastmem_exclusive_access = false; // if this set true native buffer works but only 1-3 fps, weird
-    config.recompile_on_exclusive_fastmem_failure = false;
+    config.fastmem_exclusive_access = false; // if this and below set true native buffer works but only 1-3 fps, weird
+    config.recompile_on_exclusive_fastmem_failure = false; // this one
     config.hook_hint_instructions = true;
   //  config.enable_cycle_counting = false;
     config.global_monitor = monitor;
     config.coprocessors[15] = cp15;
     config.processor_id = core_id;
-    config.optimizations = cpu_opt ? Dynarmic::all_safe_optimizations : Dynarmic::no_optimizations;  
     config.wall_clock_cntpct = true;
     
     if(cpu_unsafe){
-        config.recompile_on_fastmem_failure = true;
-        config.page_table_pointer_mask_bits = 3;
+    //    config.recompile_on_exclusive_fastmem_failure = true;
+    //    config.recompile_on_fastmem_failure = true;
+        config.page_table_pointer_mask_bits = true;
         config.absolute_offset_page_table = false;
+        config.optimizations = Dynarmic::unsafe_optimizations;
         config.unsafe_optimizations = true;
     } else {
         config.recompile_on_fastmem_failure = false;
+        config.optimizations = cpu_opt ? Dynarmic::all_safe_optimizations : Dynarmic::no_optimizations;  
         config.unsafe_optimizations = false;
     }
     
