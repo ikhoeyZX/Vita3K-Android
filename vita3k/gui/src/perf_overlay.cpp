d4fab1c07d6d8ad4c391b80f914c16d3497f8e2d
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-09-23 21:32:17 +0800

    Update perf_overlay.cpp

--- vita3k/gui/src/perf_overlay.cpp
+++ vita3k/gui/src/perf_overlay.cpp
@@ -23,12 +23,12 @@ namespace gui {
 static const ImVec2 PERF_OVERLAY_PAD = ImVec2(12.f, 12.f);
 static const ImVec4 PERF_OVERLAY_BG_COLOR = ImVec4(0.282f, 0.239f, 0.545f, 0.8f);
 
-static ImVec2 get_perf_pos(ImVec2 window_size, EmuEnvState &emuenv) {
-    const auto TOP = emuenv.viewport_pos.y - PERF_OVERLAY_PAD.y;
-    const auto LEFT = emuenv.viewport_pos.x - PERF_OVERLAY_PAD.x;
-    const auto CENTER = emuenv.viewport_pos.x + (emuenv.viewport_size.x / 2.f) - (window_size.x / 2.f);
-    const auto RIGHT = emuenv.viewport_pos.x + emuenv.viewport_size.x - window_size.x + PERF_OVERLAY_PAD.x;
-    const auto BOTTOM = emuenv.viewport_pos.y + emuenv.viewport_size.y - window_size.y + PERF_OVERLAY_PAD.y;
+static ImVec2 get_perf_pos(ImVec2 window_size, EmuEnvState &emuenv, ImVec2 scale) {
+    const float TOP = PERF_OVERLAY_PAD.y * scale.y;
+    const float LEFT = PERF_OVERLAY_PAD.x * scale.x;
+    const float CENTER = ImGui::GetIO().DisplaySize.x / 2.0 - (window_size.x / 2.f);
+    const float RIGHT = ImGui::GetIO().DisplaySize.x - window_size.x + PERF_OVERLAY_PAD.x * scale.x;
+    const float BOTTOM = ImGui::GetIO().DisplaySize.y - window_size.y + PERF_OVERLAY_PAD.y * scale.y;
 
     switch (emuenv.cfg.performance_overlay_position) {
     case TOP_CENTER: return ImVec2(CENTER, TOP);
@@ -50,6 +50,7 @@ void draw_perf_overlay(GuiState &gui, EmuEnvState &emuenv) {
     const ImVec2 RES_SCALE(VIEWPORT_SIZE.x / emuenv.res_width_dpi_scale, VIEWPORT_SIZE.y / emuenv.res_height_dpi_scale);
     const ImVec2 SCALE(RES_SCALE.x * emuenv.dpi_scale, RES_SCALE.y * emuenv.dpi_scale);
 
+    const auto SCALE = ImVec2(RES_SCALE.x * emuenv.dpi_scale, RES_SCALE.y * emuenv.dpi_scale);
     const auto SCALED_FONT_SIZE = ImGui::GetFontSize() * (0.7f * RES_SCALE.y);
     const auto FONT_SCALE = SCALED_FONT_SIZE / ImGui::GetFontSize();
 
@@ -60,7 +61,7 @@ void draw_perf_overlay(GuiState &gui, EmuEnvState &emuenv) {
     const auto MAX_TEXT_HEIGHT_SCALED = SCALED_FONT_SIZE + (emuenv.cfg.performance_overlay_detail >= MEDIUM ? SCALED_FONT_SIZE + (ImGui::GetStyle().ItemSpacing.y * 2.f) : 0.f);
     const ImVec2 WINDOW_SIZE(MAX_TEXT_WIDTH_SCALED + TOTAL_WINDOW_PADDING.x, MAX_TEXT_HEIGHT_SCALED + TOTAL_WINDOW_PADDING.y);
     const ImVec2 MAIN_WINDOW_SIZE(WINDOW_SIZE.x + TOTAL_WINDOW_PADDING.x, WINDOW_SIZE.y + TOTAL_WINDOW_PADDING.y + (emuenv.cfg.performance_overlay_detail == MAXIMUM ? WINDOW_SIZE.y : 0.f));
-    const auto WINDOW_POS = get_perf_pos(MAIN_WINDOW_SIZE, emuenv);
+    const auto WINDOW_POS = get_perf_pos(MAIN_WINDOW_SIZE, emuenv, SCALE);
     
     ImGui::SetNextWindowSize(MAIN_WINDOW_SIZE);
     ImGui::SetNextWindowPos(WINDOW_POS);
