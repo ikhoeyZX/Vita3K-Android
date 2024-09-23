eaf09046398a1d32f565af3b39167729a9f0ca8f
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-09-24 02:28:24 +0800

    Update controls_dialog.cpp

--- vita3k/gui/src/controls_dialog.cpp
+++ vita3k/gui/src/controls_dialog.cpp
@@ -148,13 +148,14 @@ void draw_controls_dialog(GuiState &gui, EmuEnvState &emuenv) {
     if (ImGui::Checkbox("Show gamepad overlay ingame", &emuenv.cfg.enable_gamepad_overlay))
         config::serialize_config(emuenv.cfg, emuenv.cfg.config_path);
 
-    
-    ImGui::Checkbox(lang["sensor_enable"].c_str(), &emuenv.cfg.tiltsens);
-    SetTooltipEx(lang["sensors_description"].c_str());
-    if (emuenv.cfg.tiltsens){
-        ImGui::Checkbox(lang["invert_gyro"].c_str(), &emuenv.cfg.invert_gyro);
-        SetTooltipEx(lang["invert_gyro_description"].c_str());
-    }        
+    if(emuenv.cfg.enable_gamepad_overlay){
+       ImGui::Checkbox(lang["sensor_enable"].c_str(), &emuenv.cfg.tiltsens);
+       SetTooltipEx(lang["sensors_description"].c_str());
+       if (emuenv.cfg.tiltsens){
+           ImGui::Checkbox(lang["invert_gyro"].c_str(), &emuenv.cfg.invert_gyro);
+           SetTooltipEx(lang["invert_gyro_description"].c_str());
+       }
+    }
     
     const char *overlay_edit_text = overlay_editing ? "Hide Gamepad Overlay" : "Modify Gamepad Overlay";
     ImGui::SetCursorPosX((ImGui::GetWindowWidth() / 2.f) - (gmpd / 2.f));
