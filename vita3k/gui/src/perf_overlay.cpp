6177f33bd829789d87582571cc55c7df10a2a40f
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-09-23 22:09:47 +0800

    Update perf_overlay.cpp

--- vita3k/gui/src/perf_overlay.cpp
+++ vita3k/gui/src/perf_overlay.cpp
@@ -48,7 +48,6 @@ void draw_perf_overlay(GuiState &gui, EmuEnvState &emuenv) {
 
     const ImVec2 VIEWPORT_SIZE(emuenv.viewport_size.x, emuenv.viewport_size.y);
     const ImVec2 RES_SCALE(VIEWPORT_SIZE.x / emuenv.res_width_dpi_scale, VIEWPORT_SIZE.y / emuenv.res_height_dpi_scale);
-    const ImVec2 SCALE(RES_SCALE.x * emuenv.dpi_scale, RES_SCALE.y * emuenv.dpi_scale);
 
     const auto SCALE = ImVec2(RES_SCALE.x * emuenv.dpi_scale, RES_SCALE.y * emuenv.dpi_scale);
     const auto SCALED_FONT_SIZE = ImGui::GetFontSize() * (0.7f * RES_SCALE.y);
