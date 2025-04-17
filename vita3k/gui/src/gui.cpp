90e717336e842233e9103cf33369431a62aad806
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-04-17 13:51:37 +0800

    Update gui.cpp

--- vita3k/gui/src/gui.cpp
+++ vita3k/gui/src/gui.cpp
@@ -231,12 +231,18 @@ static void init_font(GuiState &gui, EmuEnvState &emuenv) {
         gui.vita_font = io.Fonts->AddFontFromFileTTF(fs_utils::path_to_utf8(latin_fw_font_path).c_str(), font_config.SizePixels, &font_config, latin_range);
         font_config.MergeMode = true;
 
+	    const auto sys_lang = static_cast<SceSystemParamLang>(emuenv.cfg.sys_lang);
+                const bool is_chinese = (sys_lang == SCE_SYSTEM_PARAM_LANG_CHINESE_T) || (sys_lang == SCE_SYSTEM_PARAM_LANG_CHINESE_S);
+                // When the system language is Chinese, the Chinese fonts should be loaded before the Japanese fonts
+                // So that the CJK characters can be displayed in Chinese glyphs
+                if (is_chinese)
+                    io.Fonts->AddFontFromFileTTF(fs_utils::path_to_utf8(fw_font_path / "cn0.pvf").c_str(), font_config.SizePixels, &font_config, chinese_range);
+	    
         io.Fonts->AddFontFromFileTTF(fs_utils::path_to_utf8(fw_font_path / "jpn0.pvf").c_str(), font_config.SizePixels, &font_config, japanese_and_extra_ranges.Data);
 
-        const auto sys_lang = static_cast<SceSystemParamLang>(emuenv.cfg.sys_lang);
         if (emuenv.cfg.asia_font_support || (sys_lang == SCE_SYSTEM_PARAM_LANG_KOREAN))
             io.Fonts->AddFontFromFileTTF(fs_utils::path_to_utf8(fw_font_path / "kr0.pvf").c_str(), font_config.SizePixels, &font_config, korean_range);
-        if (emuenv.cfg.asia_font_support || (sys_lang == SCE_SYSTEM_PARAM_LANG_CHINESE_T) || (sys_lang == SCE_SYSTEM_PARAM_LANG_CHINESE_S))
+        if (emuenv.cfg.asia_font_support && !is_chinese)
             io.Fonts->AddFontFromFileTTF(fs_utils::path_to_utf8(fw_font_path / "cn0.pvf").c_str(), font_config.SizePixels, &font_config, chinese_range);
         font_config.MergeMode = false;
 
@@ -268,7 +274,7 @@ static void init_font(GuiState &gui, EmuEnvState &emuenv) {
             
 
             const auto sys_lang = static_cast<SceSystemParamLang>(emuenv.cfg.sys_lang);
-            if (sys_lang == SCE_SYSTEM_PARAM_LANG_CHINESE_S) {
+            if (!emuenv.cfg.initial_setup || (sys_lang == SCE_SYSTEM_PARAM_LANG_CHINESE_T) || (sys_lang == SCE_SYSTEM_PARAM_LANG_CHINESE_S))
 		const std::vector<uint8_t> font_source = fs_utils::read_asset_raw(fs::path(default_font_path / "NotoSerifCJK-Regular.ttc")); // Built-in Android font
 
                 if (!font_source.empty()) {
