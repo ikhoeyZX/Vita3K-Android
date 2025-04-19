b61fe272037de6beac4e5932151b581b1afa0532
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-04-20 01:56:32 +0800

    Update surface_cache.cpp

--- vita3k/renderer/src/vulkan/surface_cache.cpp
+++ vita3k/renderer/src/vulkan/surface_cache.cpp
@@ -1027,7 +1027,7 @@ Framebuffer &VKSurfaceCache::retrieve_framebuffer_handle(MemState &mem, SceGxmCo
 }
 
 bool VKSurfaceCache::check_for_surface(MemState &mem, Address source_address, CallbackRequestFunction &callback, Address target_address) {
-    if (!state.features.enable_memory_mapping || state.disable_surface_sync)
+    if (!state.features.support_memory_mapping || state.disable_surface_sync)
         return false;
 
     if (vector_utils::find_index(cpu_surfaces_changed, source_address) != -1) {
@@ -1111,7 +1111,7 @@ bool VKSurfaceCache::check_for_surface(MemState &mem, Address source_address, Ca
 
 ColorSurfaceCacheInfo *VKSurfaceCache::perform_surface_sync() {
     // surface sync is supported only if memory mapping is enabled
-    if (!state.features.enable_memory_mapping)
+    if (!state.features.support_memory_mapping)
         return nullptr;
 
     if (last_written_surface == nullptr || !*last_written_surface->need_surface_sync)
