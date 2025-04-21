c2bdbd2788a5aebb21fe5505c91ce1b3c9132645
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-04-21 21:46:27 +0800

    Update renderer.cpp

--- vita3k/renderer/src/vulkan/renderer.cpp
+++ vita3k/renderer/src/vulkan/renderer.cpp
@@ -1183,12 +1183,14 @@ bool VKState::map_memory(MemState &mem, Ptr<void> address, uint32_t size) {
         device.bindBufferMemory(mapped_buffer, device_memory, 0);
 
         vk::BufferDeviceAddressInfoKHR address_info{
-            .buffer = mapped_buffer
+            .buffer = buffer.buffer
         };
-        const uint64_t buffer_address = device.getBufferAddress(address_info);
 
-        add_external_mapping(mem, address.address(), size, reinterpret_cast<uint8_t *>(mapped_location));
-        mapped_memories[address.address()] = { address.address(), ExternalBuffer{ device_memory, buffer }, mapped_buffer, size, buffer_address };
+        const uint64_t buffer_address = device.getBufferAddress(address_info);
+        const vk::Buffer mapped_buffer = buffer.buffer;
+
+	add_external_mapping(mem, address.address(), size, static_cast<uint8_t *>(buffer.mapped_data));
+	mapped_memories[address.address()] = { address.address(), std::move(buffer), mapped_buffer, size, buffer_address };
 #else
         LOG_ERROR("Native buffer is only supported on Android!\n");
 #endif
