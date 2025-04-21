80422ea3c12ff814870fbcae8401f832b96a3e4f
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-04-21 23:45:47 +0800

    Update renderer.cpp

--- vita3k/renderer/src/vulkan/renderer.cpp
+++ vita3k/renderer/src/vulkan/renderer.cpp
@@ -1189,7 +1189,7 @@ bool VKState::map_memory(MemState &mem, Ptr<void> address, uint32_t size) {
 
 	const uint64_t buffer_address = device.getBufferAddress(address_info);
 	    
-	add_external_mapping(mem, address.address(), size, static_cast<uint8_t *>(buffer->mapped_data));
+	add_external_mapping(mem, address.address(), size, static_cast<uint8_t *>(mapped_location));
 	mapped_memories[address.address()] = MappedMemory{
 	        .mapped_address = address.address(),
 	        .external_buffer = std::move(buffer),
