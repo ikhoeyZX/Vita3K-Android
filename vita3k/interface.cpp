cb6359e9aa5a3acfba72f3a495a0daf35c79a274
ikhoeyZX <79812109+ikhoeyZX@users.noreply.github.com>
2025-04-27 00:36:14 +0800

    update

--- vita3k/interface.cpp
+++ vita3k/interface.cpp
@@ -561,9 +561,9 @@ static void take_screenshot(EmuEnvState &emuenv) {
     fs::create_directories(save_folder);
 
     const auto img_format = emuenv.cfg.screenshot_format == JPEG ? ".jpg" : ".png";
-    time_t currentTime = time(nullptr);
-    struct tm* timeInfo = localtime(&currentTime);
-    const fs::path save_file = save_folder / fmt::format("{}_{:%Y-%m-%d-%H%M%OS}{}", string_utils::remove_special_chars(emuenv.current_app_title), timeInfo, img_format);
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+    const fs::path save_file = save_folder / fmt::format("{}_{:%Y-%m-%d-%H%M%OS}{}", string_utils::remove_special_chars(emuenv.current_app_title), tm, img_format);
 
     constexpr int quality = 85; // google recommended value
     bool screenshot_ok = false;
