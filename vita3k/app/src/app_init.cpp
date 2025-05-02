2e9a7131be40ea9c6ba443bb7e611760558f451d
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-05-03 00:32:34 +0800

    Update app_init.cpp

--- vita3k/app/src/app_init.cpp
+++ vita3k/app/src/app_init.cpp
@@ -500,7 +500,17 @@ bool init(EmuEnvState &state, const Root &root_paths) {
     state.window = WindowPtr(SDL_CreateWindow(window_title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, state.res_width_dpi_scale, state.res_height_dpi_scale, window_type | SDL_WINDOW_RESIZABLE), SDL_DestroyWindow);
 
     if (!state.window) {
-        LOG_ERROR("SDL failed to create window!");
+        LOG_ERROR("SDL failed to create window! check your hardware or config!");
+        if(state.cfg.backend_renderer == "Vulkan"){
+           error_dialog("SDL failed to create window!\nnDoes your driver or GPU support Vulkan?\napp will changed to OpenGL ES render", nullptr);
+           state.cfg.backend_renderer = "OpenGL";
+        }else{
+           error_dialog("SDL failed to create window!\nDoes your GPU support OpenGL ES 3.2?\napp will changed to Vulkan render", nullptr);
+           state.cfg.backend_renderer = "Vulkan";
+        }
+        state.cfg.boot_fail = true;
+        config::serialize_config(state.cfg, state.cfg.config_path);
+        SDL_Quit();
         return false;
     }
 
