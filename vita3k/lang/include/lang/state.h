cb13e6a53d0b958e636dca5efaaebd6ea8d4a3ff
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-05-03 01:09:29 +0800

    Update state.h

--- vita3k/lang/include/lang/state.h
+++ vita3k/lang/include/lang/state.h
@@ -691,6 +691,8 @@ struct LangState {
             { "live_area_screen_description", "Check the box to open the Live Area by default when clicking on an application\nIf disabled, right click on an application to open it." },
             { "stretch_the_display_area", "Stretch The Display Area" },
             { "stretch_the_display_area_description", "Check the box to enlarge the display area to fit the screen size." },
+            { "native_screen", "Use fullscreen mode instead dpi scalling" },
+            { "native_screen_description", "Check the box to set sdl match screen size (no dpi scale)." },
             { "apps_list_grid", "Grid Mode" },
             { "apps_list_grid_description", "Check the box to set the app list to grid mode." },
             { "skip_lockscreen", "Skip lockscreen area" },
