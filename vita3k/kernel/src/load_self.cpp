79413da345d2a4107b26f95b56566924c4cf0355
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-05-10 19:35:42 +0800

    Update load_self.cpp

--- vita3k/kernel/src/load_self.cpp
+++ vita3k/kernel/src/load_self.cpp
@@ -82,8 +82,7 @@ static bool load_var_imports(const uint32_t *nids, const Ptr<uint32_t> *entries,
             constexpr auto STUB_SYMVAL = 0xDEADBEEF;
             LOG_DEBUG("\tNID NOT FOUND {} ({}) at {}, setting to stub value {}", log_hex(nid), name, log_hex(entry.address()), log_hex(STUB_SYMVAL));
 
-            auto nid_value = nid;  
-            auto alloc_name = fmt::format("Stub var import reloc symval, NID {} ({})", log_hex(nid_value), name.c_str());
+            auto alloc_name = fmt::format("Stub var import reloc symval, NID {} ({})", log_hex(nid), static_cast<char*>(name));
             auto stub_symval_ptr = Ptr<uint32_t>(alloc(mem, 4, alloc_name.c_str()));
             *stub_symval_ptr.get(mem) = STUB_SYMVAL;
 
