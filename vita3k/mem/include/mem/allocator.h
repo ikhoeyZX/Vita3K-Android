6fcc027c969266b472713bf8cc91205b55930fdc
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-05-13 11:07:26 +0800

    Update allocator.h

--- vita3k/mem/include/mem/allocator.h
+++ vita3k/mem/include/mem/allocator.h
@@ -33,8 +33,8 @@ public:
 
     void set_maximum(const std::size_t total_bits);
 
-    int allocate_from(const std::uint32_t start_offset, int &size, const bool best_fit = false);
-    int allocate_at(const std::uint32_t start_offset, int size);
+    int allocate_from(const std::uint32_t start_offset, std::uint32_t &size, const bool best_fit = false);
+    int allocate_at(const std::uint32_t start_offset, std::uint32_t size);
     void free(const std::uint32_t offset, const int size);
     void reset();
 
