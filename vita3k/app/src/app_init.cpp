494401fa6c10bd271251b0303a903c0d85f1f5d6
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-07-02 13:55:58 +0800

    revert app_init.cpp

--- vita3k/app/src/app_init.cpp
+++ vita3k/app/src/app_init.cpp
@@ -502,11 +502,33 @@ bool init(EmuEnvState &state, const Root &root_paths) {
 #endif // ifndef __arm__
     }
 #endif // ifdef android
-    
-state.window = WindowPtr(SDL_CreateWindow(window_title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, state.res_width_dpi_scale, state.res_height_dpi_scale, window_type | SDL_WINDOW_RESIZABLE), SDL_DestroyWindow);
 
+    state.window = WindowPtr(SDL_CreateWindow(window_title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, state.res_width_dpi_scale, state.res_height_dpi_scale, window_type | SDL_WINDOW_RESIZABLE), SDL_DestroyWindow);
     if (!state.window) {
-        LOG_ERROR("SDL failed to create window!");
+        LOG_ERROR("SDL failed to create window!\n Reason:{}\n disabling some feature!", SDL_GetError());
+        SDL_ClearError();
+        window_type = 0;
+        if(state.cfg.backend_renderer == "OpenGL")
+            window_type = SDL_WINDOW_OPENGL;
+        else
+            window_type = SDL_WINDOW_VULKAN;
+            
+        state.window = WindowPtr(SDL_CreateWindow(window_title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, state.res_width_dpi_scale, state.res_height_dpi_scale, window_type), SDL_DestroyWindow);
+    }
+        
+    if (!state.window) {
+        LOG_ERROR("SDL still fail to create window!\n Reason: {}\n check your hardware or config!", SDL_GetError());
+        SDL_ClearError();
+        if(state.cfg.backend_renderer == "OpenGL"){
+           error_dialog("SDL failed to create window!\nDoes your GPU support OpenGL ES 3.2?\napp will exit and changed to Vulkan render", nullptr);
+           state.cfg.backend_renderer = "Vulkan";
+        }else{
+            error_dialog("SDL failed to create window!\nnDoes your driver or GPU support Vulkan?\napp will exit and changed to OpenGL ES render", nullptr);
+            state.cfg.backend_renderer = "OpenGL";
+        }
+        state.cfg.boot_fail = true;
+        config::serialize_config(state.cfg, state.cfg.config_path);
+        SDL_Quit();
         return false;
     }
 
