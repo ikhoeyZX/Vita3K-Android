28a79d864d4caefc5ab9867646b17f1220d235e7
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-07-15 01:19:43 +0800

    Update state.h

--- vita3k/audio/include/audio/state.h
+++ vita3k/audio/include/audio/state.h
@@ -19,8 +19,6 @@
 
 #include <util/types.h>
 
-#include <SDL_audio.h>
-
 #include <functional>
 #include <map>
 #include <memory>
@@ -30,17 +28,16 @@
 #define SCE_AUDIO_OUT_MAX_VOL 32768 //!< Maximum output port volume
 #define SCE_AUDIO_VOLUME_0DB SCE_AUDIO_OUT_MAX_VOL //!< Maximum output port volume
 
-typedef std::shared_ptr<SDL_AudioStream> AudioStreamPtr;
 typedef std::function<void(SceUID)> ResumeAudioThread;
 
 struct AudioOutPort {
     // Channel range from 0 - 32768
-    uint16_t left_channel_volume = SCE_AUDIO_VOLUME_0DB;
-    uint16_t right_channel_volume = SCE_AUDIO_VOLUME_0DB;
+    int left_channel_volume = SCE_AUDIO_VOLUME_0DB;
+    int right_channel_volume = SCE_AUDIO_VOLUME_0DB;
     // Volume range from 0 to 1
     float volume = 1.0f;
     // length of the buffer for each call
-    int32_t len_bytes = 0;
+    int len_bytes = 0;
     // number of microseconds a buffer lasts for
     uint64_t len_microseconds = 0;
     // last time sceAudioOutOutput was called with this port (timestamp in microseconds)
@@ -51,29 +48,15 @@ struct AudioOutPort {
     int len = 0;
     int freq = 0;
     int mode = 0;
-
-    std::mutex mutex;
-    // stream to get the data
-    AudioStreamPtr stream;
-    // thread currently waiting for the audio to be processed
-    SceUID thread = -1;
 };
 
 typedef std::shared_ptr<AudioOutPort> AudioOutPortPtr;
 typedef std::map<int, AudioOutPortPtr> AudioOutPortPtrs;
 
 struct AudioInPort {
-    SDL_AudioDeviceID id;
+    void *id = nullptr;
     bool running = false;
-    int32_t len_bytes = 0;
-};
-
-struct AudioSpec {
-    int freq;
-    // number of samples to be processed per callback per channel
-    uint32_t nb_samples = 0;
-    // value for silence
-    uint8_t silence;
+    int len_bytes = 0;
 };
 
 struct ThreadState;
@@ -81,19 +64,8 @@ struct AudioState;
 
 // abstract class that need to be overloaded with an audio implementation
 class AudioAdapter {
-private:
-    // buffer used to mix audio
-    std::vector<uint8_t> temp_buffer;
-
-protected:
-    AudioState &state;
-    // are we using a single stream and mixing everything inside or multiple streams?
-    bool single_stream = true;
-
 public:
-    // called by subclasses once they get called by their implementation callback
-    // stream points to the location where we need to write state.ro.len_bytes bytes
-    void audio_callback(uint8_t *stream, int len_bytes);
+    AudioState &state;
 
     AudioAdapter(AudioState &audio_state)
         : state(audio_state) {}
@@ -104,21 +76,20 @@ public:
     virtual void audio_output(ThreadState &thread, AudioOutPort &out_port, const void *buffer) {}
     virtual void set_volume(AudioOutPort &out_port, float volume) {}
     virtual void switch_state(const bool pause) {}
-
+    virtual int get_rest_sample(AudioOutPort &out_port) { return 0; };
     friend struct AudioState;
 };
 
 struct AudioState {
-    AudioSpec spec;
-    // the adapter must be before out_ports for the destructors to work correctly
+    //  the adapter must be before out_ports for the destructors to work correctly
     std::unique_ptr<AudioAdapter> adapter;
     std::mutex mutex;
-    uint16_t next_port_id = 1;
+    int next_port_id = 1;
     AudioOutPortPtrs out_ports;
     AudioInPort in_port;
     ResumeAudioThread resume_thread;
     std::string audio_backend;
-    float global_volume;
+    float global_volume = 1;
 
     bool init(const ResumeAudioThread &resume_thread, const std::string &adapter_name);
     void set_backend(const std::string &adapter_name);
@@ -127,4 +98,5 @@ struct AudioState {
     void set_volume(AudioOutPort &out_port, float volume);
     void set_global_volume(float volume);
     void switch_state(const bool pause);
+    int get_rest_sample(AudioOutPort &out_port);
 };
