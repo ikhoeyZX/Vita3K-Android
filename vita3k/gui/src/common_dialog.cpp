3f00203ef9f2cf275bb0e9b4bdda12e568706d57
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-07-15 01:51:57 +0800

    Update common_dialog.cpp

--- vita3k/gui/src/common_dialog.cpp
+++ vita3k/gui/src/common_dialog.cpp
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2024 Vita3K team
+// Copyright (C) 2025 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@
 #include <dialog/state.h>
 #include <util/string_utils.h>
 
-#include <SDL.h>
+#include <SDL3/SDL_timer>
 
 namespace gui {
 static void draw_ime_dialog(DialogState &common_dialog, float FONT_SCALE) {
