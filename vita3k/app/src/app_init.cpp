6b1c3b92298550beea44abd1de5a418f8b86b8ef
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-07-27 02:40:23 +0800

    Update app_init.cpp

--- vita3k/app/src/app_init.cpp
+++ vita3k/app/src/app_init.cpp
@@ -438,7 +438,7 @@ bool init(EmuEnvState &state, const Root &root_paths) {
     
     if(SDL_GetAndroidSDKVersion() >= 30 && !state.cfg.native_screen) {
         window_type |= SDL_WINDOW_HIGH_PIXEL_DENSITY;
-        float dpi = SDL_GetDisplayContentScale(SDL_GetDisplayForWindow(state.window.get()));
+        float dpi = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay()));
         if(dpi == 0.0f){
            LOG_ERROR("Failed to scaling dpi");
         }else{
@@ -492,8 +492,9 @@ bool init(EmuEnvState &state, const Root &root_paths) {
     }
 #endif // ifdef android
 
-    state.window = WindowPtr(SDL_CreateWindow(window_title, DEFAULT_RES_WIDTH * state.dpi_scale, DEFAULT_RES_HEIGHT * state.dpi_scale, window_type | SDL_WINDOW_RESIZABLE), SDL_DestroyWindow);
-   // state.window = WindowPtr(SDL_CreateWindow(window_title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, state.res_width_dpi_scale, state.res_height_dpi_scale, window_type | SDL_WINDOW_RESIZABLE), SDL_DestroyWindow);
+    LOG_TRACE("create sdl3 window");
+    state.window = WindowPtr(SDL_CreateWindow(window_title, state.res_width_dpi_scale, state.res_height_dpi_scale, window_type | SDL_WINDOW_RESIZABLE), SDL_DestroyWindow);
+    // state.window = WindowPtr(SDL_CreateWindow(window_title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, state.res_width_dpi_scale, state.res_height_dpi_scale, window_type | SDL_WINDOW_RESIZABLE), SDL_DestroyWindow);
     if (!state.window) {
         LOG_ERROR("SDL failed to create window!\n Reason:{}\n disabling some feature!", SDL_GetError());
         SDL_ClearError();
@@ -522,6 +523,7 @@ bool init(EmuEnvState &state, const Root &root_paths) {
         return false;
     }
 
+    LOG_TRACE("create window OK");
     // initialize the renderer first because we need to know if we need a page table
     if (!state.cfg.console) {
         if (renderer::init(state.window.get(), state.renderer, state.backend_renderer, state.cfg, root_paths, state.libadreno)) {
