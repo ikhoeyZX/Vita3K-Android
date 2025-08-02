637f6d467b40f7fd719484971fa6a54c3ac5df43
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-08-03 01:43:47 +0800

    reverte screen_render.cpp

--- vita3k/renderer/src/gl/screen_render.cpp
+++ vita3k/renderer/src/gl/screen_render.cpp
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2025 Vita3K team
+// Copyright (C) 2024 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -78,8 +78,8 @@ bool ScreenRenderer::init(const fs::path &static_assets) {
     );
     glEnableVertexAttribArray(uvAttrib);
 
-    glClearColor(32.0f / 255.0f, 178.0f / 255.0f, 170.0f / 255.0f, 1.0f);
-    glClearDepth(1.0);
+    glClearColor(0.125490203f, 0.698039234f, 0.666666687f, 1.0f);
+    glClearDepthf(1.0);
 
     return true;
 }
@@ -128,7 +128,7 @@ void ScreenRenderer::render(const SceFVector2 &viewport_pos, const SceFVector2 &
         static_cast<GLsizei>(viewport_size.y));
 
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClearDepth(1.0);
+    glClearDepthf(1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     // should not be needed, but just in case
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
