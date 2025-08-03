55bcd87d33513ed7053bdc7e20a03227ad9dbf04
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-08-04 02:21:19 +0800

    Update usse_utilities.h

--- vita3k/shader/include/shader/usse_utilities.h
+++ vita3k/shader/include/shader/usse_utilities.h
@@ -98,8 +98,8 @@ void make_for_loop(spv::Builder &b, spv::Id iterator, spv::Id initial_value_ite,
 
     b.setBuildPoint(&blocks.head);
 
-    spv::Id compare_result = b.createBinOp(spv::OpSLessThan, b.makeBoolType(), b.createLoad(iterator, spv::NoPrecision), iterator_limit);
-
+    spv::Id compare_result = b.createBinOp(spv::Op::SLessThan, b.makeBoolType(), b.createLoad(iterator, spv::NoPrecision), iterator_limit);
+    
     b.createLoopMerge(&blocks.merge, &blocks.continue_target, spv::LoopControlMaskNone, {});
     b.createConditionalBranch(compare_result, &blocks.body, &blocks.merge);
 
