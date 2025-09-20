1912bc36f6e3935942d2f8bf80664bf50103f324
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-09-20 22:18:27 +0800

    Update SceLibKernel.cpp

--- vita3k/modules/SceLibKernel/SceLibKernel.cpp
+++ vita3k/modules/SceLibKernel/SceLibKernel.cpp
@@ -628,7 +628,8 @@ EXPORT(SceUID, sceIoOpen, const char *file, const int flags, const SceMode mode)
         return RET_ERROR(SCE_ERROR_ERRNO_EINVAL);
     }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    // does this cause bug?
+    // std::this_thread::sleep_for(std::chrono::milliseconds(20));
 
     LOG_INFO("Opening file: {}", file);
     return open_file(emuenv.io, file, flags, emuenv.pref_path, export_name);
