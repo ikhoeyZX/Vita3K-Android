3c84a2cde3aea8a2fe692c479da7c36c98342c06
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-09-26 00:39:03 +0800

    Update gxp.cpp

--- vita3k/gxm/src/gxp.cpp
+++ vita3k/gxm/src/gxp.cpp
@@ -115,16 +115,16 @@ GenericParameterType parameter_generic_type(const SceGxmProgramParameter &parame
 
 std::string parameter_name(const SceGxmProgramParameter &parameter) {
     std::string full_name = parameter.name();
-    LOG_TRACE(" GXP parameter_name = {}", full_name);
     const std::size_t dot_pos = full_name.find_first_of('.');
     const bool is_struct_type = dot_pos != std::string::npos;
     std::replace(full_name.begin(), full_name.end(), '.', '_');
 
+/*
     // replace brackets by underscores as on some drivers (Adreno) they cause the shader to crash
     std::replace(full_name.begin(), full_name.end(), '[', '_');
     std::replace(full_name.begin(), full_name.end(), ']', '_');
-
-    /*
+*/
+    
     if (is_struct_type) {
         // An example is abc[5].var where abc is array of struct, which will be transformed to abc_5_var
         // In case of abc[5].var[2] where var is an array, this will be turned into abc_5_var[2]
@@ -139,9 +139,8 @@ std::string parameter_name(const SceGxmProgramParameter &parameter) {
             }
         }
     }
-*/
-    LOG_TRACE(" GXP parameter_name after replace = {}", full_name);
-    return full_name;
+
+    
 }
 
 std::string parameter_struct_name(const SceGxmProgramParameter &parameter) {
