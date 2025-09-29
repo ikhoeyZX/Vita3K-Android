560e033b15bc71e1fd14b3e415d5c373d50cabc9
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-09-30 01:32:14 +0800

    Update SceProcessmgr.cpp

--- vita3k/modules/SceProcessmgr/SceProcessmgr.cpp
+++ vita3k/modules/SceProcessmgr/SceProcessmgr.cpp
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2024 Vita3K team
+// Copyright (C) 2025 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -23,6 +23,7 @@
 
 #include <util/safe_time.h>
 
+#include <util/log.h>
 #include <util/tracy.h>
 TRACY_MODULE_NAME(SceProcessmgr);
 
@@ -101,7 +102,11 @@ EXPORT(int, sceKernelCDialogSetLeaseLimit) {
 
 EXPORT(int, sceKernelCallAbortHandler, uint32_t param1, uint32_t param2) {
     TRACY_FUNC(sceKernelCallAbortHandler, param1, param2);
-    return UNIMPLEMENTED();
+    LOG_TRACE("UNIMPLEMENTED FEATURE!");
+    LOG_TRACE("param1 VAL = {}", param1);
+    LOG_TRACE("param2 VAL = {}", param2);
+    
+    return CALL_EXPORT(abort);
 }
 
 EXPORT(int, sceKernelGetCurrentProcess) {
