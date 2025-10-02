2076f5e963f2e36f9d9780d4cf9558b705136c60
ikhoeyZX <79812109+ikhoeyZX@users.noreply.github.com>
2025-10-02 16:19:50 +0800

    x

--- vita3k/renderer/src/vulkan/renderer.cpp
+++ vita3k/renderer/src/vulkan/renderer.cpp
@@ -1412,22 +1412,22 @@ std::vector<std::string> VKState::get_stencil_list() {
 
     std::vector<vk::Format> candidates = { vk::Format::eD32Sfloat, vk::Format::eD32SfloatS8Uint, vk::Format::eD24UnormS8Uint,  vk::Format::eD16UnormS8Uint,  vk::Format::eD16Unorm };
 
-    std::vector<std:string> result;
+    std::vector<std::string> result;
 
       for ( vk::Format format : candidates ) {
         vk::FormatProperties props = physical_device.getFormatProperties( format );
 
         if ( props.optimalTilingFeatures & vk::FormatFeatureFlagBits::eDepthStencilAttachment )
-          result.push_back(vk::to_sring(format));
+          result.push_back(vk::to_string(format));
 
       }
 
       // if not found use default instead
       if(result.empty()){
-          return "vk::Format::eD24UnormS8Uint";
+          result.push_back(vk::to_string("vk::Format::eD24UnormS8Uint");
         }
 
-    return format;
+    return result;
 }
 
 
