1bf3b572d30dee87748d092bf9ca475df4926fce
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-10-04 02:06:02 +0800

    Update renderer.cpp

--- vita3k/renderer/src/vulkan/renderer.cpp
+++ vita3k/renderer/src/vulkan/renderer.cpp
@@ -1414,7 +1414,7 @@ std::vector<std::string> VKState::get_vulkan_feature_list(int type) {
 		
 	switch(type) {
 		case 0:
-			const auto present_modes = physical_device.getSurfacePresentModesKHR(surface);
+			const auto present_modes = physical_device.getSurfacePresentModesKHR(screen_renderer.surface);
             
 			for ( vk::PresentModeKHR format : present_modes ) {
 				result.push_back(vk::to_string(format));
