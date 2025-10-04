cf82c6ce4cbacb869963a2ccdd4a86e1288aab0f
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-10-04 20:37:08 +0800

    Update renderer.cpp

--- vita3k/renderer/src/vulkan/renderer.cpp
+++ vita3k/renderer/src/vulkan/renderer.cpp
@@ -1419,31 +1419,32 @@ std::vector<std::string> VKState::get_vulkan_feature_list(int type) {
 				result.push_back(vk::to_string(format));
 				
 	        // if not found use default instead
-            if ( result.empty() ) {
+            if ( result.empty() ) 
                 result.push_back(vk::to_string(vk::PresentModeKHR::eMailbox));
-			}
+			
 			return result;
 	        break;
-		
+
+/*
 	    case 1: 
 			std::vector<vk::Format> candidates = { vk::Format::eD32Sfloat, vk::Format::eD32SfloatS8Uint, vk::Format::eD24UnormS8Uint,  vk::Format::eD16UnormS8Uint,  vk::Format::eD16Unorm };
 			
-			for ( vk::Format format : candidates ) {
-                vk::FormatProperties props = physical_device.getFormatProperties( format );
-
+			for ( vk::Format format : candidates )
+				{
+				  vk::FormatProperties props = physical_device.getFormatProperties( format );
+				}
             if ( props.optimalTilingFeatures & vk::FormatFeatureFlagBits::eDepthStencilAttachment )
                result.push_back(vk::to_string(format));
 	        }
 	        // if not found use default instead
-            if ( result.empty() ) {
+            if ( result.empty() ) 
                 result.push_back(vk::to_string(vk::Format::eD24UnormS8Uint));
-            }
+
 	        break;
-		
-	    default: {
+*/
+	    default: 
 			result.push_back("INVALID");
 		    break;
-		}
 	}
 	
 	return result;
