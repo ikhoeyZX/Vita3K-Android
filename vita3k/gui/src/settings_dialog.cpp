12c09902dd26ade123e60d37790b8e661dfe1f52
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-10-05 22:44:55 +0800

    Update settings_dialog.cpp

--- vita3k/gui/src/settings_dialog.cpp
+++ vita3k/gui/src/settings_dialog.cpp
@@ -981,7 +981,7 @@ void draw_settings_dialog(GuiState &gui, EmuEnvState &emuenv) {
         ImGui::Spacing();
         const std::vector<std::string> vk_surface_list_str = emuenv.renderer->get_vulkan_feature_list(0);
 
-        static std::vector<const char *> vk_surface_list;
+        std::vector<const char *> vk_surface_list;
         for (const auto &vk_surface : vk_surface_list_str)
             vk_surface_list.push_back(vk_surface.c_str());
 
@@ -997,7 +997,7 @@ void draw_settings_dialog(GuiState &gui, EmuEnvState &emuenv) {
         
         const std::vector<std::string> stencil_list_str = emuenv.renderer->get_vulkan_feature_list(1);
 
-        static std::vector<const char *> stencil_list;
+        std::vector<const char *> stencil_list;
         for (const auto &stencil : stencil_list_str)
             stencil_list.push_back(stencil.c_str());
 
