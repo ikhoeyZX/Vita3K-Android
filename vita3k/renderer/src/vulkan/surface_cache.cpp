98b9ba3f4b220b4dae6a4cb8d9815d3bcf3e3f9c
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-10-05 23:32:03 +0800

    Update surface_cache.cpp

--- vita3k/renderer/src/vulkan/surface_cache.cpp
+++ vita3k/renderer/src/vulkan/surface_cache.cpp
@@ -262,7 +262,7 @@ SurfaceRetrieveResult VKSurfaceCache::retrieve_color_surface_for_framebuffer(Mem
     vk::ImageUsageFlags surface_usages = vk::ImageUsageFlagBits::eTransferDst | vk::ImageUsageFlagBits::eTransferSrc | vk::ImageUsageFlagBits::eColorAttachment | vk::ImageUsageFlagBits::eSampled | vk::ImageUsageFlagBits::eInputAttachment;
     if (state.features.support_shader_interlock)
         surface_usages |= vk::ImageUsageFlagBits::eStorage;
-    image.init_image(surface_usages, vkutil::default_comp_mapping, image_create_flags, image_info_pNext);
+    image.init_image(surface_usages, vkutil::default_comp_mapping, image_create_flags, image_info_pNext, deep_stencil_use);
 
     // do it in the prerender if we read from this texture in the same scene (although this would be useless)
     vk::CommandBuffer cmd_buffer = context->prerender_cmd;
@@ -688,7 +688,8 @@ SurfaceRetrieveResult VKSurfaceCache::retrieve_depth_stencil_for_framebuffer(Sce
 
     image.width = width;
     image.height = height;
-    image.format = vk::Format::eD24UnormS8Uint;
+//    image.format = vk::Format::eD24UnormS8Uint;
+    image.format = deep_stencil_use;
     image.layout = vkutil::ImageLayout::Undefined;
     image.init_image(vk::ImageUsageFlagBits::eDepthStencilAttachment | vk::ImageUsageFlagBits::eTransferDst | vk::ImageUsageFlagBits::eTransferSrc | vk::ImageUsageFlagBits::eSampled);
 
@@ -838,7 +839,8 @@ std::optional<TextureLookupResult> VKSurfaceCache::retrieve_depth_stencil_as_tex
             vk::ImageViewCreateInfo view_info{
                 .image = cached_info.texture.image,
                 .viewType = vk::ImageViewType::e2D,
-                .format = vk::Format::eD24UnormS8Uint,
+    //            .format = vk::Format::eD24UnormS8Uint,
+                .format = deep_stencil_use,
                 .components = {},
                 .subresourceRange = range
             };
@@ -861,7 +863,8 @@ std::optional<TextureLookupResult> VKSurfaceCache::retrieve_depth_stencil_as_tex
         return TextureLookupResult{
             img_view,
             vkutil::ImageLayout::DepthStencilReadOnly,
-            vk::Format::eD24UnormS8Uint
+//            vk::Format::eD24UnormS8Uint
+            deep_stencil_use
         };
     }
 
@@ -883,7 +886,8 @@ std::optional<TextureLookupResult> VKSurfaceCache::retrieve_depth_stencil_as_tex
         // no compatible read surface found
 
         DepthSurfaceView read_only{
-            .depth_view = vkutil::Image(width, height, vk::Format::eD24UnormS8Uint),
+          // .depth_view = vkutil::Image(width, height, vk::Format::eD24UnormS8Uint),
+            .depth_view = vkutil::Image(width, height, deep_stencil_use),
             .scene_timestamp = 0,
             .delta_col = delta_col_samples,
             .delta_row = delta_row_samples,
@@ -907,7 +911,8 @@ std::optional<TextureLookupResult> VKSurfaceCache::retrieve_depth_stencil_as_tex
         vk::ImageViewCreateInfo view_info{
             .image = read_only.depth_view.image,
             .viewType = vk::ImageViewType::e2D,
-            .format = vk::Format::eD24UnormS8Uint,
+       //     .format = vk::Format::eD24UnormS8Uint,
+            .format = deep_stencil_use,
             .components = {},
             .subresourceRange = range
         };
