df4268203544190311de61905cd4a887dda43028
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-10-21 01:33:25 +0800

    Update mp3.cpp

--- vita3k/codec/src/mp3.cpp
+++ vita3k/codec/src/mp3.cpp
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2024 Vita3K team
+// Copyright (C) 2025 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -202,8 +202,8 @@ Mp3DecoderState::Mp3DecoderState(uint32_t channels) {
 }
 
 Mp3DecoderState::~Mp3DecoderState() {
-    avcodec_free_context(context);
-    av_free(context);
+    avcodec_free_context(&context);
+//    av_free(context);
 
-    context = nullptr;
+//    context = nullptr;
 }
