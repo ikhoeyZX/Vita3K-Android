e9cb7b3073b594df2d0993f48472e10ff74c0e94
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-10-25 01:29:06 +0800

    Update creation.cpp

--- vita3k/renderer/src/vulkan/creation.cpp
+++ vita3k/renderer/src/vulkan/creation.cpp
@@ -304,15 +304,6 @@ bool create(std::unique_ptr<FragmentProgram> &fp, VKState &state, const SceGxmPr
                 | vk::ColorComponentFlagBits::eA
         };
     }else {
-        vk::ColorComponentFlags color_mask{};
-        if (blend->colorMask & SCE_GXM_COLOR_MASK_R)
-            color_mask |= vk::ColorComponentFlagBits::eR;
-        if (blend->colorMask & SCE_GXM_COLOR_MASK_G)
-            color_mask |= vk::ColorComponentFlagBits::eG;
-        if (blend->colorMask & SCE_GXM_COLOR_MASK_B)
-            color_mask |= vk::ColorComponentFlagBits::eB;
-        if (blend->colorMask & SCE_GXM_COLOR_MASK_A)
-            color_mask |= vk::ColorComponentFlagBits::eA;
         // default values, only blendEnable and colorWriteMask are useful
         fp_vk->blending = vk::PipelineColorBlendAttachmentState{
             .blendEnable = VK_FALSE,
@@ -322,7 +313,10 @@ bool create(std::unique_ptr<FragmentProgram> &fp, VKState &state, const SceGxmPr
             .srcAlphaBlendFactor = vk::BlendFactor::eOne,
             .dstAlphaBlendFactor = vk::BlendFactor::eZero,
             .alphaBlendOp = vk::BlendOp::eAdd,
-            .colorWriteMask = color_mask
+            .colorWriteMask = vk::ColorComponentFlagBits::eR
+                | vk::ColorComponentFlagBits::eG
+                | vk::ColorComponentFlagBits::eB
+                | vk::ColorComponentFlagBits::eA
         };
     }
 
