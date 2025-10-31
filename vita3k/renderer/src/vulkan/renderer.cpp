55ebe4baf2dd39fc032492351a1bdec8afdfe086
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-10-31 23:21:58 +0800

    Update renderer.cpp

--- vita3k/renderer/src/vulkan/renderer.cpp
+++ vita3k/renderer/src/vulkan/renderer.cpp
@@ -1164,7 +1164,7 @@ bool VKState::map_memory(MemState &mem, Ptr<void> address, uint32_t size) {
 
         vk::DeviceMemory device_memory;
         // prefer this extension
-        if (support_android_buffer_import) {
+/*        if (support_android_buffer_import) {
             const vk::AndroidHardwareBufferPropertiesANDROID hardware_props = device.getAndroidHardwareBufferPropertiesANDROID(*buffer);
 
             uint32_t mapped_memory_type = find_suitable_mapped_type(hardware_props.memoryTypeBits);
@@ -1180,9 +1180,8 @@ bool VKState::map_memory(MemState &mem, Ptr<void> address, uint32_t size) {
             };
             device_memory = device.allocateMemory(alloc_info.get());
 			LOG_TRACE("ALLOC SIZE: {}", size);
-			LOG_TRACE("mapped_memory_type {}", vk::to_string(mapped_memory_type));
 			LOG_TRACE("DEVICE MEMORY NATIVE BUFFER: {}", static_cast<uint64_t>(device_memory));
-        } else {
+*///        } else {
             const native_handle_t *handle = _AHardwareBuffer_getNativeHandle(buffer);
             if (handle == nullptr || handle->numFds == 0 || handle->data[0] == -1) {
                 LOG_ERROR("Failed to get native handle");
@@ -1203,7 +1202,7 @@ bool VKState::map_memory(MemState &mem, Ptr<void> address, uint32_t size) {
                     .flags = vk::MemoryAllocateFlagBits::eDeviceAddress }
             };
             device_memory = device.allocateMemory(alloc_info.get());
-        }
+//        }
 
         vk::StructureChain<vk::BufferCreateInfo, vk::ExternalMemoryBufferCreateInfoKHR> buffer_info{
             vk::BufferCreateInfo{
