e1876f6365551f3a088f4b5bfa053095deebe293
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-11-01 19:06:29 +0800

    Update app_init.cpp

--- vita3k/app/src/app_init.cpp
+++ vita3k/app/src/app_init.cpp
@@ -47,9 +47,7 @@
 #include <SDL_vulkan.h>
 
 #ifdef ANDROID
-#include <SDL_hints.h>
-#include <SDL_messagebox.h>
-#include <SDL_system.h>
+#include <SDL.h>
 #include <boost/range/iterator_range.hpp>
 #include <jni.h>
 
