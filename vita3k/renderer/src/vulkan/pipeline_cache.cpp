393002bf42ec802c0c73b47080a42bb44f69d544
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-11-11 23:20:48 +0800

    Update pipeline_cache.cpp

--- vita3k/renderer/src/vulkan/pipeline_cache.cpp
+++ vita3k/renderer/src/vulkan/pipeline_cache.cpp
@@ -833,14 +833,15 @@ vk::Pipeline PipelineCache::compile_pipeline(SceGxmPrimitiveType type, vk::Rende
         vk::DynamicState::eStencilCompareMask,
         vk::DynamicState::eStencilReference,
         vk::DynamicState::eStencilWriteMask,
-        vk::DynamicState::eDepthBias,
+        vk::DynamicState::eDepthBias
 
-        vk::DynamicState::eBlendConstants,
+      /*  vk::DynamicState::eBlendConstants,
         vk::DynamicState::eDepthBounds,
         vk::DynamicState::ePrimitiveTopology,
         vk::DynamicState::eViewportWithCount,
         vk::DynamicState::eScissorWithCount,
         vk::DynamicState::eStencilOp
+    */
     };
     vk::PipelineDynamicStateCreateInfo dynamic_info{};
     dynamic_info.setDynamicStates(dynamic_states);
