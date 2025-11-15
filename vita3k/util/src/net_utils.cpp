336160a828911fcf6b82d057b5afe6c072628f57
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-11-16 02:30:16 +0800

    Update net_utils.cpp

--- vita3k/util/src/net_utils.cpp
+++ vita3k/util/src/net_utils.cpp
@@ -29,8 +29,6 @@
 #include <ifaddrs.h>
 #include <net/if.h>
 #include <netinet/in.h>
-#else
-#include <curl/curl.h> 
 #endif
 
 #include <condition_variable>
@@ -337,7 +335,7 @@ bool socketSetBlocking(int sockfd, bool blocking) {
 }
 
 std::string get_web_response(const std::string &url) {
-    auto curl = curl_easy_init();
+    CURL curl = curl_easy_init();
     if (!curl)
         return {};
 
