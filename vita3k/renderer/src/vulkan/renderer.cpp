171aa243963a6555483c4178bd09a42d4e0a3d98
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-12-07 16:36:17 +0800

    Update renderer.cpp

--- vita3k/renderer/src/vulkan/renderer.cpp
+++ vita3k/renderer/src/vulkan/renderer.cpp
@@ -851,12 +851,8 @@ bool VKState::create(SDL_Window *window, std::unique_ptr<renderer::State> &state
 	        vk_idx = 2;
 		}else if (config_vk_mapping == "Immediate"){
 	        vk_idx = 3;
-		}else if (config_vk_mapping == "SharedDemandRefresh"){
-	        vk_idx = 4;
-		}else if (config_vk_mapping == "SharedContinuousRefresh"){
-	        vk_idx = 5;
 		}else if (config_vk_mapping == "FifoLatestReadyEXT"){
-	        vk_idx = 6;
+	        vk_idx = 4;
 	    }else {
 	        vk_idx = 0; // Mailbox
 	    }
