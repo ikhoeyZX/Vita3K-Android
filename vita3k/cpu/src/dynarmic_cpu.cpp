0a6d1d190ec33532f60effe2679162f9d62a042b
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-12-18 00:32:56 +0800

    Update dynarmic_cpu.cpp

--- vita3k/cpu/src/dynarmic_cpu.cpp
+++ vita3k/cpu/src/dynarmic_cpu.cpp
@@ -308,8 +308,8 @@ std::unique_ptr<Dynarmic::A32::Jit> DynarmicCPU::make_jit() {
         config.only_detect_misalignment_via_page_table_on_page_boundary = true;
     }
     if (!log_mem && cpu_opt) {
-        config.fastmem_pointer = std::bit_cast<uintptr_t>(parent->mem->memory.get());
-       // config.fastmem_pointer = std::bit_cast<uintptr_t>(parent->mem->page_table.get());
+       // config.fastmem_pointer = std::bit_cast<uintptr_t>(parent->mem->memory.get());
+        config.fastmem_pointer = std::bit_cast<uintptr_t>(parent->mem->page_table.get());
     }
     config.optimizations = cpu_opt ? Dynarmic::all_safe_optimizations : Dynarmic::no_optimizations;  
     config.fastmem_exclusive_access = false; // if this and below set true native buffer works but only 1-3 fps, weird
