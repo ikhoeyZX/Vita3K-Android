edf87af901108cf17a410d7d03f0e6b539c2fbc8
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-12-24 12:19:59 +0800

    Update functions.h

--- vita3k/mem/include/mem/functions.h
+++ vita3k/mem/include/mem/functions.h
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2024 Vita3K team
+// Copyright (C) 2025 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -42,9 +42,11 @@ Address alloc(MemState &state, uint32_t size, const char *name, Address start_ad
 Address alloc_aligned(MemState &state, uint32_t size, const char *name, unsigned int alignment, Address start_addr = user_main_memory_start);
 void protect_inner(MemState &state, Address addr, uint32_t size, const MemPerm perm);
 void unprotect_inner(MemState &state, Address addr, uint32_t size);
-bool add_protect(MemState &state, Address addr, const uint32_t size, const MemPerm perm, const ProtectCallback& callback);
+bool add_protect(MemState &state, Address addr, const uint32_t size, const MemPerm perm, const ProtectCallback &callback);
+void open_access_parent_protect_segment(MemState &state, Address addr);
+void close_access_parent_protect_segment(MemState &state, Address addr);
 void add_external_mapping(MemState &mem, Address addr, uint32_t size, uint8_t *addr_ptr);
-void remove_external_mapping(MemState &mem, uint8_t *addr_ptr, uint32_t size);
+void remove_external_mapping(MemState &mem, uint8_t *addr_ptr);
 bool is_protecting(MemState &state, Address addr, MemPerm *perm = nullptr);
 bool is_valid_addr(const MemState &state, Address addr);
 bool is_valid_addr_range(const MemState &state, Address start, Address end);
