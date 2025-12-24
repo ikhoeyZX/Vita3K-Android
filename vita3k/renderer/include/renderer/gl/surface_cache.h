b802c55b1efaf578891f29be8e3b8ec469b50cff
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-12-24 19:52:10 +0800

    Update surface_cache.h

--- vita3k/renderer/include/renderer/gl/surface_cache.h
+++ vita3k/renderer/include/renderer/gl/surface_cache.h
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2024 Vita3K team
+// Copyright (C) 2025 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -27,7 +27,12 @@
 #include <unordered_map>
 #include <vector>
 
-#include <glad/glad.h>
+#if define __ARM__ || define __AARCH64__
+#include <glad/gles2.h>
+#else
+#include <glad/gl.h>
+#elif
+
 
 struct MemState;
 
@@ -74,7 +79,6 @@ struct GLColorSurfaceCacheInfo : public GLSurfaceCacheInfo {
     std::uint32_t swizzle;
 
     Ptr<void> data;
-    bool is_ping_pong_dirty;
     GLObjectArray<1> gl_texture;
     GLObjectArray<1> gl_ping_pong_texture;
     GLObjectArray<1> gl_expected_read_texture_view;
