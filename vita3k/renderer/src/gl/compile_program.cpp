df47686e71a3b8c7a3ad8cfe9fcbd901b3ef0392
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-12-26 12:04:49 +0800

    Update compile_program.cpp

--- vita3k/renderer/src/gl/compile_program.cpp
+++ vita3k/renderer/src/gl/compile_program.cpp
@@ -75,6 +75,10 @@ static SharedGLObject compile_glsl(GLenum type, const std::string &source) {
 static SharedGLObject compile_spirv(GLenum type, const std::vector<std::uint32_t> &source) {
     R_PROFILE(__func__);
 
+#if defined(__arm__) || defined(__aarch64__)
+    // not supported
+    return SharedGLObject();
+#else
     SharedGLObject shader = std::make_shared<GLObject>();
     if (!shader->init(glCreateShader(type), glDeleteShader)) {
         return SharedGLObject();
@@ -109,6 +113,7 @@ static SharedGLObject compile_spirv(GLenum type, const std::vector<std::uint32_t
     }
 
     return shader;
+#endif
 }
 
 static std::string convert_hash_to_hex(const Sha256Hash &hash) {
