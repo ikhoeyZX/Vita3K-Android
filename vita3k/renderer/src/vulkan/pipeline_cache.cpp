72e8c47c8496730be633ef97448ccfc6568693c8
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-01-01 04:49:45 +0800

    Update pipeline_cache.cpp

--- vita3k/renderer/src/vulkan/pipeline_cache.cpp
+++ vita3k/renderer/src/vulkan/pipeline_cache.cpp
@@ -779,7 +779,7 @@ vk::Pipeline PipelineCache::compile_pipeline(SceGxmPrimitiveType type, vk::Rende
     const bool use_shader_interlock = state.features.support_shader_interlock && gxm_fragment_shader->is_frag_color_used();
 
     const vk::PipelineRasterizationStateCreateInfo rasterizer{
-        .depthClampEnable = state.physical_device_features.depthClamp,
+        .depthClampEnable = state.physical_device_features.features.depthClamp,
         .polygonMode = translate_polygon_mode(record.front_polygon_mode),
         .cullMode = translate_cull_mode(record.cull_mode),
         // front face is always counter clockwise
@@ -823,13 +823,13 @@ vk::Pipeline PipelineCache::compile_pipeline(SceGxmPrimitiveType type, vk::Rende
 
     // all of these can be changed at any time using the vita graphics api (like opengl)
     // Because each one can take a lot of different values, it's better to set them as dynamic
-    const std::array dynamic_states = {
+    std::vector<vk::DynamicState> dynamic_states = {
         vk::DynamicState::eViewport,
         vk::DynamicState::eScissor,
         vk::DynamicState::eStencilCompareMask,
         vk::DynamicState::eStencilReference,
         vk::DynamicState::eStencilWriteMask,
-        vk::DynamicState::eDepthBias,
+        vk::DynamicState::eDepthBias
     };
 
     // Most mobile phones didn't support it
