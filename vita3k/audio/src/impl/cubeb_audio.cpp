e37d4e29c1089334da978dc66a7c7e7d2afc8dd1
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-01-02 12:14:45 +0800

    Update cubeb_audio.cpp

--- vita3k/audio/src/impl/cubeb_audio.cpp
+++ vita3k/audio/src/impl/cubeb_audio.cpp
@@ -77,7 +77,7 @@ CubebAudioAdapter::~CubebAudioAdapter() {
 }
 
 bool CubebAudioAdapter::init() {
-    if (cubeb_init(&cubeb_ctx, "Vita3K audio", nullptr) != CUBEB_OK) {
+    if (cubeb_init(&cubeb_ctx, "Vita3K audio", "opensl") != CUBEB_OK) {
         LOG_ERROR("Could not initialize cubeb context");
         return false;
     }
@@ -98,8 +98,10 @@ AudioOutPortPtr CubebAudioAdapter::open_port(int nb_channels, int freq, int nb_s
     };
 
     uint32_t latency;
-    cubeb_get_min_latency(cubeb_ctx, &port->spec, &latency);
-
+    if (cubeb_get_min_latency(cubeb_ctx, &port->spec, &latency) != CUBEB_OK)
+        // default value (min latency is not supported on OpenSL)
+        latency = 256;
+    
     if (cubeb_stream_init(cubeb_ctx, &port->out_stream, "Vita3K audio out", nullptr, nullptr, nullptr,
             &port->spec, latency, impl_cubeb_audio_callback, impl_cubeb_state_callback, port.get())
         != CUBEB_OK) {
