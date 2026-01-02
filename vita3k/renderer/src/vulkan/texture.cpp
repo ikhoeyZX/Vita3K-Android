4c85b60962b71c94bba39c4d32fb62f19a94e806
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-01-03 00:50:22 +0800

    Update texture.cpp

--- vita3k/renderer/src/vulkan/texture.cpp
+++ vita3k/renderer/src/vulkan/texture.cpp
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2025 Vita3K team
+// Copyright (C) 2026 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -254,7 +254,7 @@ bool VKTextureCache::init(const bool hashless_texture_cache, const fs::path &tex
     samplers.resize(max_sampler_used);
 
     // check for linear filtering on depth support
-    const vk::FormatProperties depth_linear = state.physical_device.getFormatProperties(vk::Format::eD24UnormS8Uint);
+    const vk::FormatProperties depth_linear = state.physical_device.getFormatProperties(vk::Format::eD32UnormS8Uint);
     const vk::FormatProperties x8d24_support = state.physical_device.getFormatProperties(vk::Format::eX8D24UnormPack32);
     support_depth_linear_filtering = static_cast<bool>(depth_linear.optimalTilingFeatures & vk::FormatFeatureFlagBits::eSampledImageFilterLinear);
     support_x8d24 = static_cast<bool>(x8d24_support.optimalTilingFeatures & vk::FormatFeatureFlagBits::eSampledImageFilterLinear);
@@ -279,7 +279,7 @@ bool VKTextureCache::init(const bool hashless_texture_cache, const fs::path &tex
     support_astc = static_cast<bool>(astc_support.optimalTilingFeatures & vk::FormatFeatureFlagBits::eSampledImage);
 
     LOG_INFO("max_sampler_used : {}", max_sampler_used);
-    LOG_INFO("support_d24u8_depth_linear_filtering : {}", support_depth_linear_filtering);
+    LOG_INFO("support_d32u8_depth_linear_filtering : {}", support_depth_linear_filtering);
     LOG_INFO("support_x8d24    : {}", support_x8d24);
     LOG_INFO("support_e5rgb9   : {}", support_e5rgb9);
     LOG_INFO("support_a2rgb10  : {}", support_a2rgb10);
