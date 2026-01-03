4173f6444d6e6f9bb31094fb4d2bf78a7ae3325f
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-01-04 03:38:19 +0800

    Update texture.cpp

--- vita3k/renderer/src/vulkan/texture.cpp
+++ vita3k/renderer/src/vulkan/texture.cpp
@@ -257,10 +257,8 @@ bool VKTextureCache::init(const bool hashless_texture_cache, const fs::path &tex
   //  const vk::FormatProperties depth_linear = state.physical_device.getFormatProperties(vk::Format::eD32UnormS8Uint);
     const vk::FormatProperties depth_linear = state.physical_device.getFormatProperties(state.deep_stencil_use);
     const vk::FormatProperties x8d24_support = state.physical_device.getFormatProperties(vk::Format::eX8D24UnormPack32);
-    support_depth_linear_filtering = static_cast<bool>(depth_linear.optimalTilingFeatures);
-    support_x8d24 = static_cast<bool>(x8d24_support.optimalTilingFeatures);
-//    support_depth_linear_filtering = static_cast<bool>(depth_linear.optimalTilingFeatures & vk::FormatFeatureFlagBits::eSampledImageFilterLinear);
-//    support_x8d24 = static_cast<bool>(x8d24_support.optimalTilingFeatures & vk::FormatFeatureFlagBits::eSampledImageFilterLinear);
+    support_depth_linear_filtering = static_cast<bool>(depth_linear.optimalTilingFeatures & vk::FormatFeatureFlagBits::eSampledImageFilterLinear);
+    support_x8d24 = static_cast<bool>(x8d24_support.optimalTilingFeatures & vk::FormatFeatureFlagBits::eDepthStencilAttachment);
 
     // other format
     const vk::FormatProperties e5rgb9_support = state.physical_device.getFormatProperties(vk::Format::eE5B9G9R9UfloatPack32);
