6cb24282e3188d869a0c2636f6135abe735b327f
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-01-04 03:31:18 +0800

    Update texture.cpp

--- vita3k/renderer/src/vulkan/texture.cpp
+++ vita3k/renderer/src/vulkan/texture.cpp
@@ -257,8 +257,10 @@ bool VKTextureCache::init(const bool hashless_texture_cache, const fs::path &tex
   //  const vk::FormatProperties depth_linear = state.physical_device.getFormatProperties(vk::Format::eD32UnormS8Uint);
     const vk::FormatProperties depth_linear = state.physical_device.getFormatProperties(state.deep_stencil_use);
     const vk::FormatProperties x8d24_support = state.physical_device.getFormatProperties(vk::Format::eX8D24UnormPack32);
-    support_depth_linear_filtering = static_cast<bool>(depth_linear.optimalTilingFeatures & vk::FormatFeatureFlagBits::eSampledImageFilterLinear);
-    support_x8d24 = static_cast<bool>(x8d24_support.optimalTilingFeatures & vk::FormatFeatureFlagBits::eSampledImageFilterLinear);
+    support_depth_linear_filtering = static_cast<bool>(depth_linear.optimalTilingFeatures);
+    support_x8d24 = static_cast<bool>(x8d24_support.optimalTilingFeatures);
+//    support_depth_linear_filtering = static_cast<bool>(depth_linear.optimalTilingFeatures & vk::FormatFeatureFlagBits::eSampledImageFilterLinear);
+//    support_x8d24 = static_cast<bool>(x8d24_support.optimalTilingFeatures & vk::FormatFeatureFlagBits::eSampledImageFilterLinear);
 
     // other format
     const vk::FormatProperties e5rgb9_support = state.physical_device.getFormatProperties(vk::Format::eE5B9G9R9UfloatPack32);
@@ -280,7 +282,7 @@ bool VKTextureCache::init(const bool hashless_texture_cache, const fs::path &tex
     support_astc = static_cast<bool>(astc_support.optimalTilingFeatures & vk::FormatFeatureFlagBits::eSampledImage);
 
     LOG_INFO("max_sampler_used : {}", max_sampler_used);
-    LOG_INFO("support_d32u8_depth_linear_filtering : {}", support_depth_linear_filtering);
+    LOG_INFO("support_depth_linear_filtering : {}", support_depth_linear_filtering);
     LOG_INFO("support_x8d24    : {}", support_x8d24);
     LOG_INFO("support_e5rgb9   : {}", support_e5rgb9);
     LOG_INFO("support_a2rgb10  : {}", support_a2rgb10);
