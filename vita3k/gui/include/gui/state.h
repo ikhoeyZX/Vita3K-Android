d67fbf8b57176a69bedbb75d0749b0c1d5a1d423
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-01-16 02:24:30 +0800

    Update state.h

--- vita3k/gui/include/gui/state.h
+++ vita3k/gui/include/gui/state.h
@@ -1,5 +1,5 @@
 ﻿// Vita3K emulator project
-// Copyright (C) 2024 Vita3K team
+// Copyright (C) 2026 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -151,6 +151,7 @@ struct ConfigurationMenuState {
 };
 
 struct ControlMenuState {
+    bool overlay_dialog = false;
     bool controls_dialog = false;
     bool controllers_dialog = false;
 };
