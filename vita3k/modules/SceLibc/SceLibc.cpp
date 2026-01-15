44232e3a6868dc59f61af85d976f9260c180925e
ikhoeyZX <79812109+ikhoeyZX@users.noreply.github.com>
2026-01-15 18:03:32 +0800

    update

--- vita3k/modules/SceLibc/SceLibc.cpp
+++ vita3k/modules/SceLibc/SceLibc.cpp
@@ -1347,7 +1347,7 @@ EXPORT(SceSize, strspn, const char *source1, const char *source2) {
 EXPORT(Ptr<char>, strstr, Ptr<char> *source1, Ptr<char> *source2) {
     TRACY_FUNC(strstr, source1, source2);
     Ptr<char> res = Ptr<char>();
-    char *_str = strstr(source1.get(emuenv.mem), source2.get(emuenv.mem));
+    const char *_str = strstr(source1.get(emuenv.mem), source2.get(emuenv.mem));
 
     if (_str != nullptr)
         res = Ptr<char>(_str.get(emuenv.mem);
