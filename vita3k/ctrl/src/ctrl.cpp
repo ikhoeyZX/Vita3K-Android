2b3e8237432e19c9c5ddb7fdff63430ef482cb8a
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-01-23 23:58:48 +0800

    Update ctrl.cpp

--- vita3k/ctrl/src/ctrl.cpp
+++ vita3k/ctrl/src/ctrl.cpp
@@ -73,11 +73,6 @@ Java_org_vita3k_emulator_overlay_InputOverlay_setButton(JNIEnv *env, jobject thi
         SDL_JoystickSetVirtualButton(virtual_joystick, button, value);
 }
 
-JNIEXPORT void JNICALL
-Java_org_vita3k_emulator_overlay_InputOverlay_setHideState(JNIEnv *env, jobject thiz, jboolean is_hide) {
-    gui::set_controller_overlay_state(4);
-}
-
 }
 #endif
 
