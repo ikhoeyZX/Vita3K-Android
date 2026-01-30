bebc0ee5780b83602d4e63b377d87fcd14838e16
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-01-30 12:36:56 +0800

    Update state.h

--- vita3k/renderer/include/renderer/vulkan/state.h
+++ vita3k/renderer/include/renderer/vulkan/state.h
@@ -106,6 +106,7 @@ struct VKState : public renderer::State {
     // support for the VK_KHR_uniform_buffer_standard_layout extension, needed for memory mapping and texture viewport
     bool support_standard_layout = false;
     bool support_rasterized_order_access = false;
+    bool support_color_a2rgb10 = false;
 
 #ifdef ANDROID
 // SDK > 26
