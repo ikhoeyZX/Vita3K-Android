aebab2c411a318c8377405e006f2625908fee4ae
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-02-23 23:47:33 +0800

    Update imgui_impl_sdl_vulkan.cpp

--- vita3k/gui/src/imgui_impl_sdl_vulkan.cpp
+++ vita3k/gui/src/imgui_impl_sdl_vulkan.cpp
@@ -781,8 +781,8 @@ IMGUI_API bool ImGui_ImplSdlVulkan_CreateDeviceObjects(ImGui_VulkanState &state)
             .descriptorCount = TextureState::nb_descriptor_sets + 1
         };
         vk::DescriptorPoolCreateInfo pool_info{
-            .maxSets = TextureState::nb_descriptor_sets + 1,
             .flags = vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet,
+            .maxSets = TextureState::nb_descriptor_sets + 1,
         };
         pool_info.setPoolSizes(pool_size);
         state.DescriptorPool = vk_state.device.createDescriptorPool(pool_info);
