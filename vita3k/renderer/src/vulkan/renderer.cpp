50fed9368eeb33c98eb25d7affcf33cd45118d0b
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-03-21 23:51:34 +0800

    Update renderer.cpp

--- vita3k/renderer/src/vulkan/renderer.cpp
+++ vita3k/renderer/src/vulkan/renderer.cpp
@@ -298,7 +298,6 @@ VKState::VKState(int gpu_idx)
 }
 
 bool VKState::init() {
-	shader::CURRENT_VERSION = cfg.set_spirv;
     shader_version = fmt::format("v{}", shader::CURRENT_VERSION);
     return true;
 }
@@ -840,6 +839,8 @@ bool VKState::create(SDL_Window *window, std::unique_ptr<renderer::State> &state
 		}
 		if (!features.support_spirv_1_4)
 			LOG_WARN_ONCE("Your device didn't support SPIRV 1.4");
+		
+		shader::CURRENT_VERSION = cfg.set_spirv;
 
         try {
 			device = physical_device.createDevice(device_info.get<vk::DeviceCreateInfo>());
