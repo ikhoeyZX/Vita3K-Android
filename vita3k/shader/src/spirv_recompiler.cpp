e3d89b4c5d54db90098fe63bb6a44b491ec66b6b
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-03-21 21:05:55 +0800

    Update spirv_recompiler.cpp

--- vita3k/shader/src/spirv_recompiler.cpp
+++ vita3k/shader/src/spirv_recompiler.cpp
@@ -479,7 +479,7 @@ static void create_fragment_inputs(spv::Builder &b, SpirvShaderParameters &param
                 spv::Id one = b.makeFloatConstant(1.0f);
                 res_multiplier = b.createCompositeConstruct(v4z, { res_multiplier, res_multiplier, one, one });
 
-                pa_iter_var = b.createBinOp(spv::OpFDiv, v4, pa_iter_var, res_multiplier);
+                pa_iter_var = b.createBinOp(spv::OpFDiv, v4z, pa_iter_var, res_multiplier);
             } else {
                 spv::Decoration precision = get_data_type_size(pa_dtype) < 4 ? spv::DecorationRelaxedPrecision : spv::NoPrecision;
                 pa_iter_var = b.createVariable(precision, spv::StorageClassInput, pa_iter_type, pa_name.c_str());
@@ -687,9 +687,9 @@ static void create_fragment_inputs(spv::Builder &b, SpirvShaderParameters &param
 
             spv::Id coord_index_vec;
             if(query_info.coord_index == 10) // vec2
-                coord_index_vec = b.createVariable(v2z, spv::StorageClassFunction, vec2zero);
-            else // vec 4
-                coord_index_vec = b.createVariable(v4z, spv::StorageClassFunction, vec4zero);
+                coord_index_vec = b.createCompositeConstruct(v2z, vec2zero);
+                else // vec 4
+                coord_index_vec = b.createCompositeConstruct(v4z, vec4zero);
             
             coords[query_info.coord_index].first = b.createVariable(spv::NoPrecision, spv::StorageClassInput,
                 coord_index_vec, coord_name.c_str());
