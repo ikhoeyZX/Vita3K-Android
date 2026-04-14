27a1af8c89ff2d317220b60b20889a7fdbaaa1de
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-04-15 03:32:24 +0800

    Update types.h

--- vita3k/renderer/include/renderer/gl/types.h
+++ vita3k/renderer/include/renderer/gl/types.h
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2024 Vita3K team
+// Copyright (C) 2026 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -94,6 +94,7 @@ struct GLContext : public renderer::Context {
     shader::RenderFragUniformBlock current_frag_render_info{};
 
     std::vector<size_t> self_sampling_indices;
+    bool is_ssbo = false;
 
     explicit GLContext();
     ~GLContext() override = default;
