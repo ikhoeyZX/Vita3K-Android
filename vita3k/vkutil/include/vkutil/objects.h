1908a4ee13d3b5eaa64d97633ca1ea9585a62a63
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-04-25 22:06:53 +0800

    Update objects.h

--- vita3k/vkutil/include/vkutil/objects.h
+++ vita3k/vkutil/include/vkutil/objects.h
@@ -158,7 +158,7 @@ private:
 #if defined(__aarch64__) && defined(__x86_64__)
     std::vector<uint64_t> destroy_list;
 #else
-    std::vector<uint32_t> destroy_list;
+    std::vector<uintptr_t> destroy_list;
 #endif
 
 public:
@@ -173,9 +173,9 @@ public:
         destroy_list.push_back(static_cast<uint64_t>(T::objectType));
         destroy_list.push_back(std::bit_cast<uint64_t>(vk_object));
 #else
-        destroy_list.push_back(static_cast<uint32_t>(T::objectType));
+        destroy_list.push_back(static_cast<uintptr_t>(T::objectType));
         auto raw_handle = static_cast<typename T::CType>(vk_object);
-        destroy_list.push_back(reinterpret_cast<uint32_t>(raw_handle));
+        destroy_list.push_back(reinterpret_cast<uintptr_t>(raw_handle));
 #endif
         vk_object = nullptr;
     }
