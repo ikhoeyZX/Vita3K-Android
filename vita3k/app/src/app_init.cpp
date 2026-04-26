00211105e171f41e10b850c461fadc099f3b851c
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-04-26 11:59:22 +0800

    Update app_init.cpp

--- vita3k/app/src/app_init.cpp
+++ vita3k/app/src/app_init.cpp
@@ -503,9 +503,8 @@ bool late_init(EmuEnvState &state) {
     if (state.mem.use_page_table && state.kernel.cpu_backend == CPUBackend::Unicorn) {
         LOG_CRITICAL("Unicorn backend is not supported with a page table");
         state.mem.use_page_table = false;
-        LOG_INFO("Is page table disabled? = {}", state.mem.use_page_table);
     }
-
+        
     const ResumeAudioThread resume_thread = [&state](SceUID thread_id) {
         const auto thread = lock_and_find(thread_id, state.kernel.threads, state.kernel.mutex);
         const std::lock_guard<std::mutex> lock(thread->mutex);
