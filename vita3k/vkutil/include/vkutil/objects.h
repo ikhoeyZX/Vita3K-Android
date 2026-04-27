f2fa58319cd4a572e9bcd2d0f7acb3b148b828ca
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-04-27 21:55:16 +0800

    Update objects.h

--- vita3k/vkutil/include/vkutil/objects.h
+++ vita3k/vkutil/include/vkutil/objects.h
@@ -155,7 +155,8 @@ public:
 class DestroyQueue {
 private:
     vk::Device device;
-    std::vector<uint64_t> destroy_list;
+   // std::vector<uint64_t> destroy_list;
+    std::vector<vma::Allocation> destroy_list;
 
 public:
     void init(vk::Device device);
