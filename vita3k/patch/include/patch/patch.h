8c2f241731d2cb95ae3d3c83700f1ee0e7cdb7ab
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-05-07 18:52:19 +0800

    Update patch.h

--- vita3k/patch/include/patch/patch.h
+++ vita3k/patch/include/patch/patch.h
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2025 Vita3K team
+// Copyright (C) 2026 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -32,5 +32,7 @@ struct PatchHeader {
     std::string bin;
 };
 
-std::vector<Patch> get_patches(fs::path &path, const std::string &titleid, const std::string &bin);
+using Patches = std::vector<Patch>;
+
+Patches get_patches(fs::path &path, const std::string &titleid, const std::string &bin);
 Patch parse_patch(const std::string &patch);
