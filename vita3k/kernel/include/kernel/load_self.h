b785e696aa8976c7dedff7c219a5145ce1db9f65
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-05-07 15:18:03 +0800

    Update load_self.h

--- vita3k/kernel/include/kernel/load_self.h
+++ vita3k/kernel/include/kernel/load_self.h
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2024 Vita3K team
+// Copyright (C) 2026 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -27,5 +27,5 @@ struct KernelState;
 struct MemState;
 struct KernelModule;
 
-SceUID load_self(KernelState &kernel, MemState &mem, const void *self, const std::string &self_path, const fs::path &log_path, const std::vector<Patch> &patches);
+SceUID load_self(KernelState &kernel, MemState &mem, const void *self, const std::string &self_path, const fs::path &dump_path, const Patches *const patches = nullptr);
 int unload_self(KernelState &kernel, MemState &mem, KernelModule &module);
