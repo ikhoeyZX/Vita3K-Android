ff36f0875e52824b9bfb3c73196227ec5509a578
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-05-11 16:30:29 +0800

    Update unicorn_cpu.h

--- vita3k/cpu/include/cpu/impl/unicorn_cpu.h
+++ vita3k/cpu/include/cpu/impl/unicorn_cpu.h
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2024 Vita3K team
+// Copyright (C) 2026 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -102,4 +102,5 @@ public:
     void set_log_mem(bool log) override;
     bool get_log_code() override;
     bool get_log_mem() override;
+    void clear_exclusive() override;
 };
