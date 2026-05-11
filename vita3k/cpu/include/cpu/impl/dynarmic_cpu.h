f7387daeca33a220bcaadfb3a59b4c2b8ccbd798
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-05-11 14:05:21 +0800

    Update dynarmic_cpu.h

--- vita3k/cpu/include/cpu/impl/dynarmic_cpu.h
+++ vita3k/cpu/include/cpu/impl/dynarmic_cpu.h
@@ -18,13 +18,9 @@
 #pragma once
 
 #include <dynarmic/interface/A32/a32.h>
-#include <dynarmic/interface/exclusive_monitor.h>
 
 #include <cpu/functions.h>
 #include <cpu/impl/interface.h>
-#ifdef USE_UNICORN
-#include <cpu/impl/unicorn_cpu.h>
-#endif
 
 #include <memory>
 
@@ -39,23 +35,20 @@ class DynarmicCPU : public CPUInterface {
     std::unique_ptr<Dynarmic::A32::Jit> jit;
     std::unique_ptr<ArmDynarmicCallback> cb;
     std::shared_ptr<ArmDynarmicCP15> cp15;
-    Dynarmic::ExclusiveMonitor *monitor;
 
     std::size_t core_id = 0;
 
-    bool exit_request = false;
     bool halted = false;
     bool break_ = false;
 
     bool log_mem = false;
     bool log_code = false;
     bool cpu_opt;
-    bool cpu_unsafe;
 
     std::unique_ptr<Dynarmic::A32::Jit> make_jit();
 
 public:
-    DynarmicCPU(CPUState *state, std::size_t processor_id, Dynarmic::ExclusiveMonitor *monitor, bool cpu_opt, bool cpu_unsafe);
+    DynarmicCPU(CPUState *state, std::size_t processor_id, bool cpu_opt);
     ~DynarmicCPU() override;
     int run() override;
     void stop() override;
@@ -97,6 +90,9 @@ public:
     bool get_log_code() override;
     bool get_log_mem() override;
 
+    void clear_exclusive() override;
     std::size_t processor_id() const override;
     void invalidate_jit_cache(Address start, size_t length) override;
+
+    static Dynarmic::ExclusiveMonitor shared_monitor;
 };
