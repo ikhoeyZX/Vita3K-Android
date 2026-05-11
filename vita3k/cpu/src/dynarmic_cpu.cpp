1c5b4030f4c61ffeadba59d214967142e0b6b73d
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-05-11 14:09:55 +0800

    Update dynarmic_cpu.cpp

--- vita3k/cpu/src/dynarmic_cpu.cpp
+++ vita3k/cpu/src/dynarmic_cpu.cpp
@@ -21,11 +21,11 @@
 #include <util/bit_cast.h>
 #include <util/log.h>
 
-#include <mem/state.h>
 #include <mem/ptr.h>
 
-//#include <dynarmic/frontend/A32/a32_ir_emitter.h>
+#include <dynarmic/frontend/A32/a32_ir_emitter.h>
 #include <dynarmic/interface/A32/coprocessor.h>
+#include <dynarmic/interface/exclusive_monitor.h>
 
 #include <memory>
 #include <optional>
@@ -51,9 +51,6 @@ public:
 
     CallbackOrAccessOneWord CompileSendOneWord(bool two, unsigned opc1, CoprocReg CRn,
         CoprocReg CRm, unsigned opc2) override {
-        if (CRn == CoprocReg::C13 && CRm == CoprocReg::C0 && opc1 == 0 && opc2 == 3) {
-            return &tpidruro;
-        }
         return CallbackOrAccessOneWord{};
     }
 
@@ -125,19 +122,19 @@ public:
     }
 
     void PreCodeTranslationHook(bool is_thumb, Dynarmic::A32::VAddr pc, Dynarmic::A32::IREmitter &ir) override {
-       // if (cpu->log_code) {
-       //     ir.CallHostFunction(&TraceInstruction, ir.Imm64((uint64_t)this), ir.Imm64(pc), ir.Imm64(is_thumb));
-       // }
+        if (cpu->log_code) {
+            ir.CallHostFunction(&TraceInstruction, ir.Imm64((uint64_t)this), ir.Imm64(pc), ir.Imm64(is_thumb));
+        }
     }
 
     template <typename T>
     T MemoryRead(Dynarmic::A32::VAddr addr) {
         Ptr<T> ptr{ addr };
-        if (!ptr || !ptr.valid(*parent->mem) || ptr.address() < parent->mem->host_page_size) {
+        if (!ptr || !ptr.valid(*parent->mem) || ptr.address() < parent->mem->page_size) {
             LOG_ERROR("Invalid read of uint{}_t at address: 0x{:x}\n{}", sizeof(T) * 8, addr, this->cpu->save_context().description());
 
             auto pc = this->cpu->get_pc();
-            if (pc < parent->mem->host_page_size)
+            if (pc < parent->mem->page_size)
                 LOG_CRITICAL("PC is 0x{:x}", pc);
             else
                 LOG_ERROR("Executing: {}", disassemble(*parent, pc, nullptr));
@@ -170,11 +167,11 @@ public:
     template <typename T>
     void MemoryWrite(Dynarmic::A32::VAddr addr, T value) {
         Ptr<T> ptr{ addr };
-        if (!ptr || !ptr.valid(*parent->mem) || ptr.address() < parent->mem->host_page_size) {
+        if (!ptr || !ptr.valid(*parent->mem) || ptr.address() < parent->mem->page_size) {
             LOG_ERROR("Invalid write of uint{}_t at addr: 0x{:x}, val = 0x{:x}\n{}", sizeof(T) * 8, addr, value, this->cpu->save_context().description());
 
             auto pc = this->cpu->get_pc();
-            if (pc < parent->mem->host_page_size)
+            if (pc < parent->mem->page_size)
                 LOG_CRITICAL("PC is 0x{:x}", pc);
             else
                 LOG_ERROR("Executing: {}", disassemble(*parent, pc, nullptr));
@@ -206,11 +203,11 @@ public:
     template <typename T>
     bool MemoryWriteExclusive(Dynarmic::A32::VAddr addr, T value, T expected) {
         Ptr<T> ptr{ addr };
-        if (!ptr || !ptr.valid(*parent->mem) || ptr.address() < parent->mem->host_page_size) {
+        if (!ptr || !ptr.valid(*parent->mem) || ptr.address() < parent->mem->page_size) {
             LOG_ERROR("Invalid exclusive write of uint{}_t at addr: 0x{:x}, val = 0x{:x}, expected = 0x{:x}\n{}", sizeof(T) * 8, addr, value, expected, this->cpu->save_context().description());
 
             auto pc = this->cpu->get_pc();
-            if (pc < parent->mem->host_page_size)
+            if (pc < parent->mem->page_size)
                 LOG_CRITICAL("PC is 0x{:x}", pc);
             else
                 LOG_ERROR("Executing: {}", disassemble(*parent, pc, nullptr));
@@ -266,6 +263,7 @@ public:
         case Dynarmic::A32::Exception::SendEvent:
         case Dynarmic::A32::Exception::SendEventLocal:
         case Dynarmic::A32::Exception::WaitForEvent:
+            break;
         case Dynarmic::A32::Exception::Yield:
             break;
         case Dynarmic::A32::Exception::UndefinedInstruction:
@@ -300,82 +298,49 @@ public:
     }
 };
 
+Dynarmic::ExclusiveMonitor DynarmicCPU::shared_monitor(MAX_CORE_COUNT);
+
 std::unique_ptr<Dynarmic::A32::Jit> DynarmicCPU::make_jit() {
     Dynarmic::A32::UserConfig config{};
     config.arch_version = Dynarmic::A32::ArchVersion::v7;
     config.callbacks = cb.get();
-    // disable it because it make memory mapping crash
-/*    if (parent->mem->use_page_table) {
-        config.page_table = (log_mem || !cpu_opt) ? nullptr : std::bit_cast<decltype(config.page_table)>(parent->mem->page_table.get());
+    if (parent->mem->use_page_table) {
+        config.page_table = (log_mem || !cpu_opt) ? nullptr : reinterpret_cast<decltype(config.page_table)>(parent->mem->page_table.get());
         config.absolute_offset_page_table = true;
-        config.detect_misaligned_access_via_page_table = 4;
-        config.only_detect_misalignment_via_page_table_on_page_boundary = true;
+    } else if (!log_mem && cpu_opt) {
+        config.fastmem_pointer = std::bit_cast<uintptr_t>(parent->mem->memory.get());
     }
-*/
-
-    if (!log_mem && cpu_opt) {
-         config.fastmem_exclusive_access = true; 
-         config.recompile_on_exclusive_fastmem_failure = true;
-         config.fastmem_pointer = std::optional<uintptr_t>(reinterpret_cast<uintptr_t>(parent->mem->memory.get()));
-       // config.fastmem_pointer = std::bit_cast<uintptr_t>(parent->mem->memory.get());
-    }else{
-         config.fastmem_pointer = std::optional<uintptr_t>(std::nullopt);
-         config.fastmem_exclusive_access = false; // if this and below set true native buffer works but only 1-3 fps, weird
-         config.recompile_on_exclusive_fastmem_failure = false; // this one
-    }
-    
-    config.optimizations = cpu_opt ? Dynarmic::all_safe_optimizations : Dynarmic::no_optimizations;  
     config.hook_hint_instructions = true;
-    config.global_monitor = monitor;
+    config.enable_cycle_counting = false;
+    config.global_monitor = &shared_monitor;
     config.coprocessors[15] = cp15;
     config.processor_id = core_id;
-    config.wall_clock_cntpct = true;
-    
-    if(cpu_unsafe){
-        config.unsafe_optimizations = true;
-        config.optimizations |= Dynarmic::OptimizationFlag::Unsafe_UnfuseFMA;
-        config.optimizations |= Dynarmic::OptimizationFlag::Unsafe_IgnoreStandardFPCRValue;
-        config.optimizations |= Dynarmic::OptimizationFlag::Unsafe_InaccurateNaN;
-        config.optimizations |= Dynarmic::OptimizationFlag::Unsafe_IgnoreGlobalMonitor;
-    } else {
-        config.unsafe_optimizations = false;
-    }
-    
-    // Code cache size
-#if defined __aarch64__
-    config.code_cache_size =  128 * 1024 * 1024;
-#else
-    config.code_cache_size =  512 * 1024 * 1024;
-#endif
-    
+    config.optimizations = cpu_opt ? Dynarmic::all_safe_optimizations : Dynarmic::no_optimizations;
+    config.enable_cycle_counting = false;
+
     return std::make_unique<Dynarmic::A32::Jit>(config);
 }
 
-DynarmicCPU::DynarmicCPU(CPUState *state, std::size_t processor_id, Dynarmic::ExclusiveMonitor *monitor, bool cpu_opt, bool cpu_unsafe)
+DynarmicCPU::DynarmicCPU(CPUState *state, std::size_t processor_id, bool cpu_opt)
     : parent(state)
     , cb(std::make_unique<ArmDynarmicCallback>(*state, *this))
     , cp15(std::make_shared<ArmDynarmicCP15>())
-    , monitor(monitor)
     , core_id(processor_id)
-    , cpu_opt(cpu_opt)
-    , cpu_unsafe(cpu_unsafe) {
+    , cpu_opt(cpu_opt) {
     jit = make_jit();
 }
 
 DynarmicCPU::~DynarmicCPU() = default;
 
 int DynarmicCPU::run() {
-    if (!jit->IsExecuting() && exit_request == true) {
-        exit_request = false;
-        LOG_INFO("exit_request = false");
-    }
     halted = false;
     break_ = false;
     parent->svc_called = false;
     Dynarmic::HaltReason halt_reason;
     do {
         halt_reason = jit->Run();
-    } while (halt_reason == Dynarmic::HaltReason::Step || halt_reason == Dynarmic::HaltReason::CacheInvalidation);
+    } while ((halt_reason == Dynarmic::HaltReason::Step) || (halt_reason == Dynarmic::HaltReason::CacheInvalidation));
+
     return halted;
 }
 
@@ -419,11 +384,7 @@ bool DynarmicCPU::get_log_mem() {
 }
 
 void DynarmicCPU::stop() {
-    exit_request = true;
-    if (jit->IsExecuting()) {
-        jit->HaltExecution();
-        LOG_INFO("exit_request = true");
-    }
+    jit->HaltExecution();
 }
 
 uint32_t DynarmicCPU::get_reg(uint8_t idx) {
@@ -528,17 +489,6 @@ void DynarmicCPU::invalidate_jit_cache(Address start, size_t length) {
     jit->InvalidateCacheRange(start, length);
 }
 
-// TODO: proper abstraction
-ExclusiveMonitorPtr new_exclusive_monitor(int max_num_cores) {
-    return new Dynarmic::ExclusiveMonitor(max_num_cores);
-}
-
-void free_exclusive_monitor(ExclusiveMonitorPtr monitor) {
-    Dynarmic::ExclusiveMonitor *monitor_ = static_cast<Dynarmic::ExclusiveMonitor *>(monitor);
-    delete monitor_;
-}
-
-void clear_exclusive(ExclusiveMonitorPtr monitor, std::size_t core_num) {
-    Dynarmic::ExclusiveMonitor *monitor_ = static_cast<Dynarmic::ExclusiveMonitor *>(monitor);
-    monitor_->ClearProcessor(core_num);
+void DynarmicCPU::clear_exclusive() {
+    shared_monitor.ClearProcessor(core_id);
 }
