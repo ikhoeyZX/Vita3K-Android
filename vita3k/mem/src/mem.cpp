b531d9fcb77b4896b794c81a12f398c36092084d
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-05-19 19:10:21 +0800

    Update mem.cpp

--- vita3k/mem/src/mem.cpp
+++ vita3k/mem/src/mem.cpp
@@ -104,11 +104,11 @@ bool init(MemState &state, const bool use_page_table) {
 #ifdef __arm__
     state.memory = Memory(static_cast<uint8_t *>(mmap(nullptr, TOTAL_MEM_SIZE, prot, flags, fd, offset)), delete_memory);
     
-    for(int a =  TOTAL_MEM_SIZE, a > 0, a - MiB(96)) {
+    for(int a = TOTAL_MEM_SIZE; a > 0; a - MiB(96)) {
         state.memory = Memory(static_cast<uint8_t *>(mmap(nullptr, TOTAL_MEM_SIZE, prot, flags, fd, offset)), delete_memory);
     
         if (state.memory.get() == MAP_FAILED) {
-            LOG_CRITICAL("mmap failed {}, TOTAL_MEM_SIZE = {}", get_error_msg(), MiB(TOTAL_MEM_SIZE));
+            LOG_CRITICAL("mmap failed {}, TOTAL_MEM_SIZE = {} MB", get_error_msg(), MiB(TOTAL_MEM_SIZE));
         } else {
             break;
         }
@@ -117,7 +117,7 @@ bool init(MemState &state, const bool use_page_table) {
 #else
     state.memory = Memory(static_cast<uint8_t *>(mmap(preferred_address, TOTAL_MEM_SIZE, prot, flags, fd, offset)), delete_memory);
 #endif
-    LOG_INFO("Memory free: {}", mem_available(state));
+    LOG_INFO("Memory free: {} MB", MiB(mem_available(state)));
     
     if (state.memory.get() == MAP_FAILED) {
         LOG_CRITICAL("mmap failed {}", get_error_msg());
