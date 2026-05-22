e40cc760279b26c0fceb62f4f79bc4d0ae0d8788
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-05-23 03:06:29 +0800

    Update vfs.h

--- vita3k/io/include/io/vfs.h
+++ vita3k/io/include/io/vfs.h
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2025 Vita3K team
+// Copyright (C) 2026 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 #include <util/fs.h>
 #include <util/types.h>
 
-class VitaIoDevice;
+enum class VitaIoDevice : int;
 
 namespace vfs {
 
