d11e8a7644290b82ccfe156a2bef4a24a1f5aad0
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-06-13 01:00:29 +0800

    Update module_parent.h

--- vita3k/modules/include/modules/module_parent.h
+++ vita3k/modules/include/modules/module_parent.h
@@ -19,7 +19,6 @@
 
 #include <kernel/types.h>
 #include <module/bridge.h>
-#include <patch/patch.h>
 #include <util/types.h>
 
 struct CPUState;
@@ -40,7 +39,7 @@ bool has_hle_implementation(uint32_t nid);
  * \param module_path Full path of module file (with device)
  * \return UID of the loaded module object or SCE_ERROR on failure
  */
-SceUID load_module(EmuEnvState &emuenv, const std::string &module_path, const std::vector<Patch> *patches = nullptr);
+SceUID load_module(EmuEnvState &emuenv, const std::string &module_path);
 int unload_module(EmuEnvState &emuenv, SceUID module_id);
 
 uint32_t start_module(EmuEnvState &emuenv, const SceKernelModuleInfo &module, SceSize args = 0, Ptr<const void> argp = Ptr<const void>{});
@@ -54,9 +53,9 @@ uint32_t stop_module(EmuEnvState &emuenv, const SceKernelModuleInfo &module, Sce
  */
 bool load_sys_module(EmuEnvState &emuenv, SceSysmoduleModuleId module_id);
 int unload_sys_module(EmuEnvState &emuenv, SceSysmoduleModuleId module_id);
-bool load_sys_module_internal_with_arg(EmuEnvState &emuenv, SceUID thread_id, SceSysmoduleInternalModuleId module_id, SceSize args, Ptr<void> argp, int *retcode);
+bool load_sys_module_internal_with_arg(EmuEnvState &emuenv, SceSysmoduleInternalModuleId module_id, SceSize args, Ptr<void> argp, int *retcode);
 
 void load_taihen_plugins_for_title(EmuEnvState &emuenv, const std::string &titleid);
 
 Ptr<void> create_vtable(const std::vector<uint32_t> &nids, MemState &mem);
-Ptr<void> get_client_vtable(MemState &mem);
+Ptr<void> get_client_vtable(KernelState &kernel, MemState &mem);
