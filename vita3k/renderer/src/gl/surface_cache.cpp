fac7cabfbcfdcb739d5a6b12de5e8c0aba3c0dbb
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-07-08 18:22:53 +0800

    Update surface_cache.cpp

--- vita3k/renderer/src/gl/surface_cache.cpp
+++ vita3k/renderer/src/gl/surface_cache.cpp
@@ -66,7 +66,7 @@ GLuint GLSurfaceCache::retrieve_color_surface_texture_handle(const State &state,
 
     const std::uint64_t key = address.address();
     
-    LOG_TRACE("W = {}, H = {}, pixel stride = {}, base format = {}, addresses = {}, purpose = {}, swizzle = {}", width, height, pixel_stride, log_hex(base_format), key, purpose, *swizzle);
+    LOG_TRACE("W = {}, H = {}, pixel stride = {}, base format = {}, addresses = {}, purpose = {}, swizzle = {}", width, height, pixel_stride, log_hex(base_format), key, purpose, swizzle);
     
     const uint32_t original_width = width;
     const uint32_t original_height = height;
