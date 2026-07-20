7ba0f66b27cc8d99f6d61d2c9a18b091c79ced58
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-07-20 23:17:52 +0800

    Update pkg.h

--- vita3k/packages/include/packages/pkg.h
+++ vita3k/packages/include/packages/pkg.h
@@ -84,4 +84,4 @@ struct PkgEntry {
 
 bool install_pkg(const fs::path &pkg_path, EmuEnvState &emuenv, std::string &p_zRIF, const std::function<void(float)> &progress_callback = nullptr);
 
-bool decrypt_install_nonpdrm(EmuEnvState &emuenv, const fs::path &drmlicpath, const fs::path &title_path);
+bool decrypt_install_nonpdrm(EmuEnvState &emuenv, const fs::path &drmlicpath, const fs::path &title_path, const std::function<void(float)> &progress_callback = nullptr);
