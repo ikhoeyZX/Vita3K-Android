998b3cf15b70ab5fcb632abfdbf85dd34f8c3926
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-07-24 00:16:04 +0800

    disable verbose log in surface_cache.cpp

--- vita3k/renderer/src/gl/surface_cache.cpp
+++ vita3k/renderer/src/gl/surface_cache.cpp
@@ -66,7 +66,7 @@ GLuint GLSurfaceCache::retrieve_color_surface_texture_handle(const State &state,
 
     const std::uint64_t key = address.address();
     
-    LOG_TRACE("W = {}, H = {}, pixel stride = {}, addresses = {}, swizzle = {}", width, height, pixel_stride, key, swizzle);
+    // LOG_TRACE("W = {}, H = {}, pixel stride = {}, addresses = {}, swizzle = {}", width, height, pixel_stride, key, swizzle);
     
     const uint32_t original_width = width;
     const uint32_t original_height = height;
